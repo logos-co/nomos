78ab8fae5ac6a533fbc4e5732419ce23ff237139
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-03-31 19:03:10 +0200

    Replace System Time with NTP.

--- tests/src/topology/configs/time.rs
+++ tests/src/topology/configs/time.rs
@@ -1,4 +1,8 @@
-use std::{str::FromStr, time::Duration};
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    str::FromStr,
+    time::Duration,
+};
 
 use time::OffsetDateTime;
 
@@ -9,6 +13,10 @@ const CONSENSUS_SLOT_TIME_VAR: &str = "CONSENSUS_SLOT_TIME";
 pub struct GeneralTimeConfig {
     pub slot_duration: Duration,
     pub chain_start_time: OffsetDateTime,
+    pub ntp_server: String,
+    pub timeout: Duration,
+    pub local_socket: SocketAddr,
+    pub update_interval: Duration,
 }
 
 #[must_use]
@@ -19,5 +27,9 @@ pub fn default_time_config() -> GeneralTimeConfig {
     GeneralTimeConfig {
         slot_duration: Duration::from_secs(slot_duration),
         chain_start_time: OffsetDateTime::now_utc(),
+        ntp_server: String::from("185.251.115.30"), // 0.europe.pool.ntp.org
+        timeout: Duration::from_secs(5),
+        local_socket: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 31123),
+        update_interval: Duration::from_secs(16),
     }
 }
