05a0ff27e50acd7e77b6c7470231ad4c53676fb7
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-04-02 17:58:37 +0900

    refactor: introduce CryptarchiaAdapter for decoupling Synchronization with CryptarchiaConsensus (#1207)

--- nomos-services/cryptarchia-consensus/src/sync.rs
+++ nomos-services/cryptarchia-consensus/src/sync.rs
@@ -5,220 +5,40 @@ use std::{
     marker::{PhantomData, Unpin},
 };
 
+use cryptarchia_engine::Slot;
 use futures::{Stream, StreamExt};
 use itertools::Itertools;
-use nomos_core::{
-    block::Block,
-    da::blob::{info::DispersedBlobInfo, BlobSelect},
-    header::HeaderId,
-    tx::{Transaction, TxSelect},
-};
-use nomos_da_sampling::backend::DaSamplingServiceBackend;
-use nomos_mempool::backend::RecoverableMempool;
-use nomos_storage::backends::StorageBackend;
-use rand::{RngCore, SeedableRng};
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use tokio::sync::broadcast;
+use nomos_core::{block::Block, header::HeaderId};
+use serde::{de::DeserializeOwned, Serialize};
 
-use crate::{
-    blend, leadership::Leader, network, relays::CryptarchiaConsensusRelays, Cryptarchia,
-    CryptarchiaConsensus, Error,
-};
+use crate::network;
 
-#[expect(
-    clippy::type_complexity,
-    reason = "CryptarchiaConsensusRelays amount of generics."
-)]
-pub struct Synchronization<
-    NetAdapter,
-    BlendAdapter,
-    ClPool,
-    ClPoolAdapter,
-    DaPool,
-    DaPoolAdapter,
-    TxS,
-    BS,
-    Storage,
-    SamplingBackend,
-    SamplingNetworkAdapter,
-    SamplingRng,
-    SamplingStorage,
-    DaVerifierBackend,
-    DaVerifierNetwork,
-    DaVerifierStorage,
-    TimeBackend,
-    ApiAdapter,
-    RuntimeServiceId,
-> {
-    _marker: PhantomData<(
-        NetAdapter,
-        BlendAdapter,
-        ClPool,
-        ClPoolAdapter,
-        DaPool,
-        DaPoolAdapter,
-        TxS,
-        BS,
-        Storage,
-        SamplingBackend,
-        SamplingNetworkAdapter,
-        SamplingRng,
-        SamplingStorage,
-        DaVerifierBackend,
-        DaVerifierNetwork,
-        DaVerifierStorage,
-        TimeBackend,
-        ApiAdapter,
-        RuntimeServiceId,
-    )>,
+// TODO: Consider moving this to a separate crate.
+// (e.g. consensus/cryptarchia-sync/core)
+
+// TODO: Replace Tx and BlobCertificate with Block if possible.
+// This requires refactoring NetworkAdapter.
+pub struct Synchronization<Cryptarchia, Network, Tx, BlobCertificate, RuntimeServiceId> {
+    _marker: PhantomData<(Cryptarchia, Network, Tx, BlobCertificate, RuntimeServiceId)>,
 }
-impl<
-        NetAdapter,
-        BlendAdapter,
-        ClPool,
-        ClPoolAdapter,
-        DaPool,
-        DaPoolAdapter,
-        TxS,
-        BS,
-        Storage,
-        SamplingBackend,
-        SamplingNetworkAdapter,
-        SamplingRng,
-        SamplingStorage,
-        DaVerifierBackend,
-        DaVerifierNetwork,
-        DaVerifierStorage,
-        TimeBackend,
-        ApiAdapter,
-        RuntimeServiceId,
-    >
-    Synchronization<
-        NetAdapter,
-        BlendAdapter,
-        ClPool,
-        ClPoolAdapter,
-        DaPool,
-        DaPoolAdapter,
-        TxS,
-        BS,
-        Storage,
-        SamplingBackend,
-        SamplingNetworkAdapter,
-        SamplingRng,
-        SamplingStorage,
-        DaVerifierBackend,
-        DaVerifierNetwork,
-        DaVerifierStorage,
-        TimeBackend,
-        ApiAdapter,
-        RuntimeServiceId,
-    >
+
+impl<Cryptarchia, Network, Tx, BlobCertificate, RuntimeServiceId>
+    Synchronization<Cryptarchia, Network, Tx, BlobCertificate, RuntimeServiceId>
 where
-    NetAdapter: network::NetworkAdapter<RuntimeServiceId, Tx = ClPool::Item, BlobCertificate = DaPool::Item>
-        + Clone
-        + Send
-        + Sync
-        + 'static,
-    NetAdapter::Settings: Send,
-    BlendAdapter: blend::BlendAdapter<RuntimeServiceId> + Clone + Send + Sync + 'static,
-    BlendAdapter::Settings: Send,
-    ClPool: RecoverableMempool<BlockId = HeaderId> + Send + Sync + 'static,
-    ClPool::RecoveryState: Serialize + for<'de> Deserialize<'de>,
-    ClPool::Settings: Clone + Send + Sync + 'static,
-    ClPool::Item: Transaction<Hash = ClPool::Key>
-        + Debug
-        + Clone
-        + Eq
-        + Hash
-        + Serialize
-        + serde::de::DeserializeOwned
-        + Send
-        + Sync
-        + 'static,
-    ClPool::Key: Debug + Send + Sync,
-    ClPoolAdapter: nomos_mempool::network::NetworkAdapter<
-            RuntimeServiceId,
-            Payload = ClPool::Item,
-            Key = ClPool::Key,
-        > + Send
-        + Sync
-        + 'static,
-    DaPool::Item: DispersedBlobInfo<BlobId = DaPool::Key>
-        + nomos_core::da::blob::metadata::Metadata
-        + Debug
-        + Clone
-        + Eq
-        + Hash
-        + Serialize
-        + DeserializeOwned
-        + Send
-        + Sync
-        + 'static,
-    DaPool: RecoverableMempool<BlockId = HeaderId, Key = SamplingBackend::BlobId>
-        + Send
-        + Sync
-        + 'static,
-    DaPool::RecoveryState: Serialize + for<'de> Deserialize<'de>,
-    DaPool::Settings: Clone + Send + Sync + 'static,
-    DaPoolAdapter: nomos_mempool::network::NetworkAdapter<RuntimeServiceId, Key = DaPool::Key>
-        + Send
-        + Sync
-        + 'static,
-    DaPoolAdapter::Payload: DispersedBlobInfo + Into<DaPool::Item> + Debug,
-    TxS: TxSelect<Tx = ClPool::Item> + Clone + Send + Sync + 'static,
-    TxS::Settings: Send,
-    BS: BlobSelect<BlobId = DaPool::Item> + Clone + Send + Sync + 'static,
-    BS::Settings: Send,
-    Storage: StorageBackend + Send + Sync + 'static,
-    SamplingBackend: DaSamplingServiceBackend<SamplingRng> + Send,
-    SamplingBackend::Settings: Clone,
-    SamplingBackend::Share: Debug + 'static,
-    SamplingBackend::BlobId: Debug + Ord + Send + Sync + 'static,
-    SamplingNetworkAdapter: nomos_da_sampling::network::NetworkAdapter<RuntimeServiceId>,
-    SamplingRng: SeedableRng + RngCore,
-    SamplingStorage: nomos_da_sampling::storage::DaStorageAdapter<RuntimeServiceId>,
-    DaVerifierStorage: nomos_da_verifier::storage::DaStorageAdapter<RuntimeServiceId> + Send + Sync,
-    DaVerifierBackend: nomos_da_verifier::backend::VerifierBackend + Send + Sync + 'static,
-    DaVerifierBackend::Settings: Clone,
-    DaVerifierNetwork: nomos_da_verifier::network::NetworkAdapter<RuntimeServiceId> + Send + Sync,
-    DaVerifierNetwork::Settings: Clone,
-    TimeBackend: nomos_time::backends::TimeBackend,
-    TimeBackend::Settings: Clone + Send + Sync,
-    ApiAdapter: nomos_da_sampling::api::ApiAdapter + Send + Sync,
+    Tx: Serialize + DeserializeOwned + Clone + Eq + Hash + Send + 'static,
+    BlobCertificate: Serialize + DeserializeOwned + Clone + Eq + Hash + Send + 'static,
+    Cryptarchia: CryptarchiaAdapter<Tx = Tx, BlobCertificate = BlobCertificate> + Sync + Send,
+    Network: network::NetworkAdapter<RuntimeServiceId, Tx = Tx, BlobCertificate = BlobCertificate>
+        + Sync,
 {
     /// Syncs the local block tree with the peers, starting from the local tip.
     /// This covers the case where the local tip is not on the latest honest
     /// chain anymore.
-    #[expect(
-        clippy::type_complexity,
-        reason = "CryptarchiaConsensusRelays amount of generics."
-    )]
     #[expect(dead_code, reason = "Sync protocol is not integrated yet.")]
-    pub async fn initiate(
-        cryptarchia: Cryptarchia,
-        leader: &mut Leader,
-        _ledger_config: nomos_ledger::Config,
-        relays: &CryptarchiaConsensusRelays<
-            BlendAdapter,
-            BS,
-            ClPool,
-            ClPoolAdapter,
-            DaPool,
-            DaPoolAdapter,
-            NetAdapter,
-            SamplingBackend,
-            SamplingRng,
-            Storage,
-            TxS,
-            DaVerifierBackend,
-            RuntimeServiceId,
-        >,
-        block_broadcaster: &mut broadcast::Sender<Block<ClPool::Item, DaPool::Item>>,
-        network_adapter: &NetAdapter,
-    ) -> Cryptarchia {
-        let mut cryptarchia = Some(cryptarchia);
-
+    pub async fn run(
+        mut cryptarchia: Cryptarchia,
+        network: &Network,
+    ) -> Result<Cryptarchia, Box<dyn std::error::Error + Send + Sync + 'static>> {
         // Repeat the sync process until no peer has a tip ahead of the local tip,
         // because peers' tips may advance during the sync process.
         let mut rejected_blocks = HashSet::new();
@@ -230,8 +50,8 @@ where
             let mut num_blocks = 0;
 
             // TODO: handle network error
-            let mut stream = network_adapter
-                .fetch_blocks_from_slot(cryptarchia.as_ref().unwrap().tip_state().slot())
+            let mut stream = network
+                .fetch_blocks_from_slot(cryptarchia.tip_slot())
                 .await
                 .unwrap();
             while let Some(block) = stream.next().await {
@@ -245,30 +65,15 @@ where
                     continue;
                 }
 
-                match Self::process_block(
-                    cryptarchia.take().unwrap(),
-                    leader,
-                    block,
-                    relays,
-                    block_broadcaster,
-                )
-                .await
-                {
-                    Ok(c) => {
+                match cryptarchia.process_block(block).await {
+                    Ok(()) => {
                         orphan_blocks.remove(&id);
-                        cryptarchia = Some(c);
                     }
-                    Err((
-                        Error::Ledger(nomos_ledger::LedgerError::ParentNotFound(_))
-                        | Error::Consensus(cryptarchia_engine::Error::ParentMissing(_)),
-                        c,
-                    )) => {
+                    Err(CryptarchiaAdapterError::ParentNotFound) => {
                         orphan_blocks.insert(id, header.slot());
-                        cryptarchia = Some(c);
                     }
-                    Err((_, c)) => {
+                    Err(CryptarchiaAdapterError::InvalidBlock(_)) => {
                         rejected_blocks.insert(id);
-                        cryptarchia = Some(c);
                     }
                 };
             }
@@ -291,78 +96,33 @@ where
                 // backfillings (i.e. if it has been already added to the local
                 // block tree). Or, skip if it has been rejected during the
                 // previous backfillings.
-                if cryptarchia
-                    .as_ref()
-                    .unwrap()
-                    .ledger_state(&orphan_block)
-                    .is_some()
-                    || rejected_blocks.contains(&orphan_block)
-                {
+                if cryptarchia.has_block(&orphan_block) || rejected_blocks.contains(&orphan_block) {
                     continue;
                 }
 
-                match Self::backfill_fork(
-                    cryptarchia.take().unwrap(),
-                    leader,
-                    orphan_block,
-                    relays,
-                    block_broadcaster,
-                    network_adapter,
-                )
-                .await
+                if let Err((_, invalid_suffix)) =
+                    Self::backfill_fork(&mut cryptarchia, orphan_block, network).await
                 {
-                    Ok(c) => {
-                        cryptarchia = Some(c);
-                    }
-                    Err((_, c, invalid_suffix)) => {
-                        rejected_blocks.extend(invalid_suffix);
-                        cryptarchia = Some(c);
-                    }
+                    rejected_blocks.extend(invalid_suffix);
                 }
             }
         }
 
-        cryptarchia.unwrap()
+        Ok(cryptarchia)
     }
 
     /// Backfills a fork, which is absent in the local block tree
     /// by fetching blocks from the peers.
     /// During backfilling, the fork choice rule is continuously applied.
-    #[expect(
-        clippy::type_complexity,
-        reason = "CryptarchiaConsensusRelays amount of generics."
-    )]
     async fn backfill_fork(
-        cryptarchia: Cryptarchia,
-        leader: &mut Leader,
-        fork_tip: HeaderId,
-        relays: &CryptarchiaConsensusRelays<
-            BlendAdapter,
-            BS,
-            ClPool,
-            ClPoolAdapter,
-            DaPool,
-            DaPoolAdapter,
-            NetAdapter,
-            SamplingBackend,
-            SamplingRng,
-            Storage,
-            TxS,
-            DaVerifierBackend,
-            RuntimeServiceId,
-        >,
-        block_broadcaster: &mut broadcast::Sender<Block<ClPool::Item, DaPool::Item>>,
-        network_adapter: &NetAdapter,
-    ) -> Result<Cryptarchia, (Error, Cryptarchia, Vec<HeaderId>)> {
-        let mut cryptarchia = Some(cryptarchia);
-
+        cryptarchia: &mut Cryptarchia,
+        tip: HeaderId,
+        network: &Network,
+    ) -> Result<(), (CryptarchiaAdapterError, Vec<HeaderId>)> {
         let suffix = Self::find_missing_part(
             // TODO: handle network error
-            network_adapter
-                .fetch_chain_backward(fork_tip)
-                .await
-                .unwrap(),
-            cryptarchia.as_ref().unwrap(),
+            network.fetch_chain_backward(tip).await.unwrap(),
+            cryptarchia,
         )
         .await;
 
@@ -371,31 +131,17 @@ where
         let mut iter = suffix.into_iter();
         while let Some(block) = iter.next() {
             let id = block.header().id();
-            match Self::process_block(
-                cryptarchia.take().unwrap(),
-                leader,
-                block,
-                relays,
-                block_broadcaster,
-            )
-            .await
-            {
-                Ok(c) => {
-                    cryptarchia = Some(c);
-                }
-                Err((e, c)) => {
-                    return Err((
-                        e,
-                        c,
-                        std::iter::once(id)
-                            .chain(iter.map(|block| block.header().id()))
-                            .collect(),
-                    ));
-                }
+            if let Err(e) = cryptarchia.process_block(block).await {
+                return Err((
+                    e,
+                    std::iter::once(id)
+                        .chain(iter.map(|block| block.header().id()))
+                        .collect(),
+                ));
             };
         }
 
-        Ok(cryptarchia.unwrap())
+        Ok(())
     }
 
     /// Finds the point where the fork is disconnected from the local block
@@ -404,66 +150,41 @@ where
     /// divergence point of the fork in the case where the fork has been
     /// partially backfilled.
     async fn find_missing_part(
-        mut fork: Box<dyn Stream<Item = Block<ClPool::Item, DaPool::Item>> + Send + Sync + Unpin>,
+        mut fork: Box<dyn Stream<Item = Block<Tx, BlobCertificate>> + Send + Sync + Unpin>,
         cryptarchia: &Cryptarchia,
-    ) -> VecDeque<Block<ClPool::Item, DaPool::Item>> {
+    ) -> VecDeque<Block<Tx, BlobCertificate>> {
         let mut suffix = VecDeque::new();
         while let Some(block) = fork.next().await {
-            if cryptarchia.ledger_state(&block.header().id()).is_none() {
-                suffix.push_front(block);
-            } else {
+            if cryptarchia.has_block(&block.header().id()) {
                 break;
             }
+            suffix.push_front(block);
         }
         suffix
     }
+}
 
-    #[expect(
-        clippy::type_complexity,
-        reason = "CryptarchiaConsensusRelays amount of generics."
-    )]
+#[async_trait::async_trait]
+pub trait CryptarchiaAdapter {
+    // TODO: Replace Tx and BlobCertificate with Block.
+    //       This requires refactoring NetworkAdapter.
+    type Tx: Clone + Eq + Hash + 'static;
+    type BlobCertificate: Clone + Eq + Hash + 'static;
+
     async fn process_block(
-        cryptarchia: Cryptarchia,
-        leader: &mut Leader,
-        block: Block<ClPool::Item, DaPool::Item>,
-        relays: &CryptarchiaConsensusRelays<
-            BlendAdapter,
-            BS,
-            ClPool,
-            ClPoolAdapter,
-            DaPool,
-            DaPoolAdapter,
-            NetAdapter,
-            SamplingBackend,
-            SamplingRng,
-            Storage,
-            TxS,
-            DaVerifierBackend,
-            RuntimeServiceId,
-        >,
-        block_broadcaster: &mut broadcast::Sender<Block<ClPool::Item, DaPool::Item>>,
-    ) -> Result<Cryptarchia, (Error, Cryptarchia)> {
-        CryptarchiaConsensus::<
-            NetAdapter,
-            BlendAdapter,
-            ClPool,
-            ClPoolAdapter,
-            DaPool,
-            DaPoolAdapter,
-            TxS,
-            BS,
-            Storage,
-            SamplingBackend,
-            SamplingNetworkAdapter,
-            SamplingRng,
-            SamplingStorage,
-            DaVerifierBackend,
-            DaVerifierNetwork,
-            DaVerifierStorage,
-            TimeBackend,
-            ApiAdapter,
-            RuntimeServiceId,
-        >::process_block(cryptarchia, leader, block, relays, block_broadcaster)
-        .await
-    }
+        &mut self,
+        block: Block<Self::Tx, Self::BlobCertificate>,
+    ) -> Result<(), CryptarchiaAdapterError>;
+
+    fn tip_slot(&self) -> Slot;
+
+    fn has_block(&self, id: &HeaderId) -> bool;
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum CryptarchiaAdapterError {
+    #[error("Parent not found")]
+    ParentNotFound,
+    #[error("Invalid block: {0}")]
+    InvalidBlock(Box<dyn std::error::Error + Send + Sync>),
 }
