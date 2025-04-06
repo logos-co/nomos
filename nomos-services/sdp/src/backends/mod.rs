6f1db5b76a4fbff37793cf63933c232ab0161a00
Petar Radovic <petar.radovic@gmail.com>
2025-04-07 00:09:13 +0200

    sdp nomos service stub

--- nomos-services/sdp/src/backends/mod.rs
+++ nomos-services/sdp/src/backends/mod.rs
@@ -1,15 +1,20 @@
+use overwatch::overwatch::handle::OverwatchHandle;
+
 #[async_trait::async_trait]
-pub trait SdpLedgerBackend {
-    type BlockNumber;
-    type SdpMessage;
+pub trait SdpBackend<RuntimeServiceId> {
+    type BlockNumber: Clone + Send + Sync;
+    type Message: Send + Sync;
     type Error: std::error::Error + 'static + Send + Sync;
-    
+    type Settings: Clone + Send + Sync + 'static;
+
+    fn new(settings: Self::Settings, overwatch_handle: OverwatchHandle<RuntimeServiceId>) -> Self;
+
     async fn process_sdp_message(
-        &mut self, 
-        block_number: Self::BlockNumber, 
-        message: Self::SdpMessage
+        &mut self,
+        block_number: Self::BlockNumber,
+        message: Self::Message,
     ) -> Result<(), Self::Error>;
-    
+
     async fn mark_in_block(&mut self, block_number: Self::BlockNumber) -> Result<(), Self::Error>;
     fn discard_block(&mut self, block_number: Self::BlockNumber);
 }
---
+++ nomos-services/sdp/src/lib.rs
@@ -0,0 +1,118 @@
+pub mod backends;
+
+use std::fmt::Display;
+
+use async_trait::async_trait;
+use backends::SdpBackend;
+use futures::StreamExt;
+use overwatch::{
+    services::{
+        state::{NoOperator, NoState},
+        AsServiceId, ServiceCore, ServiceData,
+    },
+    OpaqueServiceStateHandle,
+};
+use services_utils::overwatch::lifecycle;
+use tokio::sync::oneshot;
+
+#[derive(Debug)]
+pub enum SdpMessage<B: SdpBackend<RuntimeServiceId>, RuntimeServiceId> {
+    Process {
+        block_number: B::BlockNumber,
+        message: B::Message,
+    },
+
+    MarkInBlock {
+        block_number: B::BlockNumber,
+        result_sender: oneshot::Sender<Result<(), B::Error>>,
+    },
+    DiscardBlock(B::BlockNumber),
+}
+
+impl<B: SdpBackend<RuntimeServiceId> + 'static + Send + Sync, RuntimeServiceId> ServiceData
+    for SdpService<B, RuntimeServiceId>
+{
+    type Settings = B::Settings;
+    type State = NoState<Self::Settings>;
+    type StateOperator = NoOperator<Self::State>;
+    type Message = SdpMessage<B, RuntimeServiceId>;
+}
+
+pub struct SdpService<B: SdpBackend<RuntimeServiceId> + Send + Sync + 'static, RuntimeServiceId> {
+    backend: B,
+    service_state: OpaqueServiceStateHandle<Self, RuntimeServiceId>,
+}
+
+#[async_trait]
+impl<B, RuntimeServiceId> ServiceCore<RuntimeServiceId> for SdpService<B, RuntimeServiceId>
+where
+    B: SdpBackend<RuntimeServiceId> + Send + Sync + 'static,
+    RuntimeServiceId: AsServiceId<Self> + Clone + Display + Send,
+{
+    fn init(
+        service_state: OpaqueServiceStateHandle<Self, RuntimeServiceId>,
+        _initstate: Self::State,
+    ) -> Result<Self, overwatch::DynError> {
+        Ok(Self {
+            backend: B::new(
+                service_state.settings_reader.get_updated_settings(),
+                service_state.overwatch_handle.clone(),
+            ),
+            service_state,
+        })
+    }
+
+    async fn run(mut self) -> Result<(), overwatch::DynError> {
+        let Self {
+            mut backend,
+            service_state:
+                OpaqueServiceStateHandle::<Self, RuntimeServiceId> {
+                    mut inbound_relay,
+                    lifecycle_handle,
+                    ..
+                },
+        } = self;
+        let mut lifecycle_stream = lifecycle_handle.message_stream();
+        let backend = &mut backend;
+        loop {
+            tokio::select! {
+                Some(msg) = inbound_relay.recv() => {
+                    Self::handle_sdp_message(msg, backend).await;
+                }
+                Some(msg) = lifecycle_stream.next() => {
+                    if lifecycle::should_stop_service::<Self, RuntimeServiceId>(&msg) {
+                        break;
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<B: SdpBackend<RuntimeServiceId> + Send + Sync + 'static, RuntimeServiceId>
+    SdpService<B, RuntimeServiceId>
+{
+    async fn handle_sdp_message(msg: SdpMessage<B, RuntimeServiceId>, backend: &mut B) {
+        match msg {
+            SdpMessage::Process {
+                block_number,
+                message,
+            } => {
+                if let Err(e) = backend.process_sdp_message(block_number, message).await {
+                    tracing::error!("Error processing SDP message: {}", e);
+                }
+            }
+            SdpMessage::MarkInBlock {
+                block_number,
+                result_sender,
+            } => {
+                let result = backend.mark_in_block(block_number).await;
+                let _ = result_sender.send(result);
+            }
+            SdpMessage::DiscardBlock(block_number) => {
+                backend.discard_block(block_number);
+            }
+        }
+    }
+}
