e9e27d59e387969437c92ba5f56862886ca7c19b
Petar Radovic <petar.radovic@gmail.com>
2025-05-05 11:24:28 +0200

    merge master

---
+++ nomos-services/sdp/src/adapters/rewards/rewards_sender.rs
@@ -0,0 +1,50 @@
+use std::fmt::Debug;
+
+use async_trait::async_trait;
+use nomos_sdp_core::{
+    ledger::{ActivityContract, ActivityContractError},
+    ActiveMessage,
+};
+
+use super::SdpRewardsAdapter;
+
+pub struct LedgerRewardsAdapter<Metadata, ContractAddress> {
+    phantom: std::marker::PhantomData<(Metadata, ContractAddress)>,
+}
+
+impl<Metadata: Send + Sync, ContractAddress: Send + Debug + Sync> SdpRewardsAdapter
+    for LedgerRewardsAdapter<Metadata, ContractAddress>
+{
+    fn new() -> Self {
+        Self {
+            phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<Metadata, ContractAddress> LedgerRewardsAdapter<Metadata, ContractAddress> {
+    #[must_use]
+    pub fn new() -> Self {
+        Self {
+            phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+#[async_trait]
+impl<Metadata, ContractAddress> ActivityContract for LedgerRewardsAdapter<Metadata, ContractAddress>
+where
+    Metadata: std::marker::Send + std::marker::Sync,
+    ContractAddress: std::marker::Send + std::fmt::Debug + std::marker::Sync,
+{
+    type ContractAddress = ContractAddress;
+    type Metadata = Metadata;
+
+    async fn mark_active(
+        &self,
+        reward_contract: Self::ContractAddress,
+        reward_message: ActiveMessage<Self::Metadata>,
+    ) -> Result<(), ActivityContractError<Self::ContractAddress>> {
+        todo!() // Implementation will go here later
+    }
+}
