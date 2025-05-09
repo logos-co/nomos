157eb5c3ed3e00fe79aae5737cb182461ae2d439
Andrus Salumets <andrus@lagrange.dev>
2025-05-09 12:54:45 +0200

    remove bounds from trait definitions

--- nomos-services/storage/src/api/chain/mod.rs
+++ nomos-services/storage/src/api/chain/mod.rs
@@ -1,14 +1,12 @@
 pub mod requests;
 
-use std::fmt::Debug;
-
 use async_trait::async_trait;
 use overwatch::DynError;
 
 #[async_trait]
 pub trait StorageChainApi {
     type HeaderId;
-    type Block: Debug;
+    type Block;
 
     async fn get_block(
         &mut self,
