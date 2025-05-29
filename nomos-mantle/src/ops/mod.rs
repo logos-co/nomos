c47143fb5370e4c5c2f6e110e60fe4fc2fefd8e5
Daniel <sanchez.quiros.daniel@gmail.com>
2025-05-29 19:28:32 +0200

    Untangle types

--- nomos-mantle/src/ops/mod.rs
+++ nomos-mantle/src/ops/mod.rs
@@ -3,8 +3,43 @@ mod opcode;
 pub(crate) mod serde_;
 
 use serde::{Deserialize, Serialize};
-#[derive(Serialize, Deserialize)]
+
+use crate::ops::{blob::BlobOp, opcode::BLOB};
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum Op {
-    Blob(blob::BlobOp),
+    Blob(
+        #[serde(serialize_with = "serde_::serialize_op_variant::<{BLOB}, BlobOp, _>")]
+        #[serde(deserialize_with = "serde_::deserialize_op_variant::<{BLOB}, BlobOp, _>")]
+        blob::BlobOp,
+    ),
+}
+
+mod tests {
+    use serde::Deserialize;
+    use serde_json::{Value, json};
+
+    use crate::ops::{Op, blob::BlobOp};
+
+    #[test]
+    fn test_serialize_deserialize_op() {
+        let zeros = json!([
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0
+        ]);
+        let payload = json!({"channel": 0, "blob": zeros, "blob_size": 0, "after_tx": Value::Null, "signer": zeros});
+        let repr = json!({"opcode": 0x01, "payload": payload});
+        println!("{:?}", serde_json::to_string(&repr).unwrap());
+        let op = Op::Blob(BlobOp {
+            channel: 0,
+            blob: [0; 32],
+            blob_size: 0,
+            after_tx: None,
+            signer: [0; 32],
+        });
+        let serialized = serde_json::to_value(&op).unwrap();
+        assert_eq!(serialized, repr);
+        let deserialized = serde_json::from_value::<Op>(repr).unwrap();
+        assert_eq!(deserialized, op);
+    }
 }
