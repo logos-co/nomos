99f7f33db1ae7aa876146e749bf11045d178bd13
Daniel <sanchez.quiros.daniel@gmail.com>
2025-05-30 13:37:32 +0200

    Add sdp active

--- nomos-mantle/src/ops/sdp.rs
+++ nomos-mantle/src/ops/sdp.rs
@@ -1,4 +1,4 @@
 pub type SDPDeclareOp = nomos_sdp_core::DeclarationMessage;
 pub type SDPWithdrawOp = nomos_sdp_core::WithdrawMessage;
 // TODO: Abstract metadata
-pub type SDPActivityOp = nomos_sdp_core::ActiveMessage<Vec<u8>>;
+pub type SDPActiveOp = nomos_sdp_core::ActiveMessage<Vec<u8>>;
