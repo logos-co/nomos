4905d4c506a073da288a2047aafac2f5da05fb46
Daniel <sanchez.quiros.daniel@gmail.com>
2025-05-30 11:11:09 +0200

    Implement channel keys

---
+++ nomos-mantle/src/ops/channel_keys.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Serialize};
+
+use crate::ops::{ChannelId, Ed25519PublicKey};
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize, Clone)]
+pub struct SetChannelKeysOp {
+    channel: ChannelId,
+    keys: Vec<Ed25519PublicKey>,
+}
