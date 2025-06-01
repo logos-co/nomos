db07eaf473817b4c036264d711d088b41cf67333
Daniel <sanchez.quiros.daniel@gmail.com>
2025-06-01 19:04:37 +0200

    Implement mantle tx gas price

--- nomos-mantle/src/tx.rs
+++ nomos-mantle/src/tx.rs
@@ -5,10 +5,21 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 
-use crate::ops::Op;
+use crate::{
+    gas::{Gas, GasConstants, GasPrice},
+    ops::Op,
+};
 
 // PLACEHOLDERS
-pub type LedgerTx = ();
+pub type LedgerTx = PhantomData<()>;
+
+impl GasPrice for LedgerTx {
+    fn gas_price<Constants: GasConstants>(&self) -> Gas {
+        // TODO: properly implement this when adding the ledger tx,
+        // for now making every tx too expensive so it would blow up its usage.
+        u64::MAX
+    }
+}
 pub type OpProof = ();
 pub type LedgerTxProof = ();
 
@@ -18,7 +29,7 @@ pub const MANTLE_HASH_VERSION: &[u8] = b"NOMOS_MANTLE_TXHASH_V1";
 pub struct MantleTx {
     pub ops: Vec<Op>,
     // temporary holder
-    pub ledger_tx: PhantomData<LedgerTx>,
+    pub ledger_tx: LedgerTx,
     pub gas_price: u64,
 }
 
@@ -28,6 +39,14 @@ impl Hash for MantleTx {
         self.ops.hash(state);
         self.gas_price.hash(state);
         self.ledger_tx.hash(state);
+    }
+}
+
+impl GasPrice for MantleTx {
+    fn gas_price<Constants: GasConstants>(&self) -> Gas {
+        let ops_gas: Gas = self.ops.iter().map(GasPrice::gas_price::<Constants>).sum();
+        let ledger_tx_gas = self.ledger_tx.gas_price::<Constants>();
+        ops_gas + ledger_tx_gas
     }
 }
 
