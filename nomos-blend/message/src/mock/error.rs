00ef2e15ea69fe3459fce584fd9335a0c32a0257
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-06-03 13:10:52 +0900

    refactor(blend): Use 'debug' log level for `MessageUnwrapError::NotAllowed`

--- nomos-blend/message/src/mock/error.rs
+++ nomos-blend/message/src/mock/error.rs
@@ -8,7 +8,4 @@ pub enum Error {
     InvalidNumberOfLayers,
     #[error("Invalid public key")]
     InvalidPublicKey,
-    #[error("Unwrapping a message is not allowed to this node")]
-    /// e.g. the message cannot be unwrapped using the private key provided
-    MsgUnwrapNotAllowed,
 }
