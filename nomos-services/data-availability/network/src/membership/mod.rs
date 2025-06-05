afae62cd876d03d9d6ec1ee92e1bc1d0e609397e
Gusto <bacvinka@gmail.com>
2025-06-05 15:27:15 +0300

    BlockNumber type from nomos-core

--- nomos-services/data-availability/network/src/membership/mod.rs
+++ nomos-services/data-availability/network/src/membership/mod.rs
@@ -4,11 +4,12 @@ pub mod handler;
 use std::collections::{HashMap, HashSet};
 
 use libp2p::PeerId;
+use nomos_core::block::BlockNumber;
 use nomos_da_network_core::SubnetworkId;
 
 pub type Assignations = HashMap<SubnetworkId, HashSet<PeerId>>;
 
 pub trait MembershipStorage {
-    fn store(&self, block_number: u64, assignations: Assignations);
-    fn get(&self, block_number: u64) -> Option<Assignations>;
+    fn store(&self, block_number: BlockNumber, assignations: Assignations);
+    fn get(&self, block_number: BlockNumber) -> Option<Assignations>;
 }
