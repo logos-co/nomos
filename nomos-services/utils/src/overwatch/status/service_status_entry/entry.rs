26d17947138b587d25a3a3028c332777ba5e60a4
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-06-05 15:32:22 +0200

    Split service status entry-related code into files.

---
+++ nomos-services/utils/src/overwatch/status/service_status_entry/entry.rs
@@ -0,0 +1,46 @@
+//! Defines the [`ServiceStatusEntry`] type, which associates a service
+//! identifier with its current [`ServiceStatus`].
+//!
+//! This module is useful for tracking the readiness or operational state of
+//! services within an Overwatch-based system. It provides an ergonomic way to
+//! collect and display service statuses, particularly when aggregating
+//! diagnostics or errors.
+
+use std::fmt::{Debug, Display, Formatter};
+
+use crate::overwatch::status::service_status_entry::status_wrapper;
+
+pub struct ServiceStatusEntry<RuntimeServiceId> {
+    id: RuntimeServiceId,
+    status: status_wrapper::ServiceStatus,
+}
+
+impl<RuntimeServiceId> ServiceStatusEntry<RuntimeServiceId> {
+    pub const fn from_overwatch(
+        id: RuntimeServiceId,
+        status: overwatch::services::status::ServiceStatus,
+    ) -> Self {
+        Self {
+            id,
+            status: status_wrapper::ServiceStatus::new(status),
+        }
+    }
+}
+
+impl<RuntimeServiceId: Display> Display for ServiceStatusEntry<RuntimeServiceId> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let Self { id, status } = self;
+        let id = id.to_string();
+        write!(f, "{id}: {status}")
+    }
+}
+
+impl<RuntimeServiceId: Display> Debug for ServiceStatusEntry<RuntimeServiceId> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let Self { id, status } = self;
+        let id = id.to_string();
+        write!(f, "ServiceStatusEntry {{ id: {id}, status: {status:?} }}")
+    }
+}
+
+impl<RuntimeServiceId: Display> std::error::Error for ServiceStatusEntry<RuntimeServiceId> {}
