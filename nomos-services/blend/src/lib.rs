9cfdd7daee5d5df205410793a199944a1b7d74c9
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-06-05 18:04:04 +0200

    Make Blend await readyness.

--- nomos-services/blend/src/lib.rs
+++ nomos-services/blend/src/lib.rs
@@ -38,6 +38,7 @@ use overwatch::{
 use rand::SeedableRng as _;
 use rand_chacha::ChaCha12Rng;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use services_utils::wait_until_services_are_ready;
 use tokio::{sync::mpsc, time};
 use tokio_stream::wrappers::{IntervalStream, UnboundedReceiverStream};
 
@@ -168,6 +169,12 @@ where
         tracing::info!(
             "Service '{}' is ready.",
             <RuntimeServiceId as AsServiceId<Self>>::SERVICE_ID
+        );
+
+        wait_until_services_are_ready!(
+            &self.service_resources_handle.overwatch_handle,
+            Some(Duration::from_millis(3000)),
+            NetworkService<_, _>
         );
 
         loop {
