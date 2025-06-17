259e7b8a92a2249d73dd28a41705a4b68b1c7fe4
Petar Radovic <petar.radovic@gmail.com>
2025-06-17 19:42:51 +0200

    rust lints

--- nomos-services/sdp/src/adapters/declaration/repository.rs
+++ nomos-services/sdp/src/adapters/declaration/repository.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 use nomos_sdp_core::{
-    ledger::{self, DeclarationsRepository, DeclarationsRepositoryError},
+    ledger::{DeclarationsRepository, DeclarationsRepositoryError},
     DeclarationInfo,
 };
 
