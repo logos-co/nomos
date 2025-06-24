f7a63268d7546a447b4cac1841aa3c0aa6aeee7d
Antonio Antonino <antonio@status.im>
2025-06-24 14:42:59 +0200

    Final refactoring

--- nomos-blend/scheduling/src/message_scheduler/round_info.rs
+++ nomos-blend/scheduling/src/message_scheduler/round_info.rs
@@ -38,7 +38,7 @@ impl Display for Round {
 /// Information can the message scheduler can yield when being polled.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RoundInfo {
-    /// The list of *unshuffled* messages to be released.
+    /// The list of messages to be released.
     pub processed_messages: Vec<OutboundMessage>,
     /// The cover message to generate, if present.
     pub cover_message: Option<CoverMessage>,
