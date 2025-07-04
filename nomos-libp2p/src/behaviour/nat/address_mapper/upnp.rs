fb309294c05d65e845f811dd372079367e9c9127
andrussal <andrus@lagrange.dev>
2025-07-04 15:54:48 +0200

    feat(nat): remove explicit error mapping

--- nomos-libp2p/src/behaviour/nat/address_mapper/upnp.rs
+++ nomos-libp2p/src/behaviour/nat/address_mapper/upnp.rs
@@ -23,7 +23,11 @@ impl MappingProtocol for UpnpProtocol {
         Self: Sized,
     {
         let gateway = igd_next::aio::tokio::search_gateway(SearchOptions::default()).await?;
-        let gateway_external_ip = gateway.get_external_ip().await?;
+
+        let gateway_external_ip = gateway
+            .get_external_ip()
+            .await
+            .map_err(|e| AddressMapperError::ExternalIpFailed(e.to_string()))?;
 
         tracing::info!("UPnP gateway found: {gateway_external_ip}");
 
