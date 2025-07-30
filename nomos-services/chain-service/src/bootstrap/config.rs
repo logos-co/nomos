594075b9f8688014be99a38b9e9cfab188575c8c
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-07-30 15:33:32 +0900

    feat(chain): IBD

--- nomos-services/chain-service/src/bootstrap/config.rs
+++ nomos-services/chain-service/src/bootstrap/config.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{collections::HashSet, hash::Hash, num::NonZeroUsize, time::Duration};
 
 use nomos_utils::bounded_duration::{MinimalBoundedDuration, SECOND};
 use serde::{Deserialize, Serialize};
@@ -6,8 +6,28 @@ use serde_with::serde_as;
 
 #[serde_with::serde_as]
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct BootstrapConfig {
+pub struct BootstrapConfig<NodeId>
+where
+    NodeId: Clone + Eq + Hash,
+{
     #[serde_as(as = "MinimalBoundedDuration<0, SECOND>")]
     pub prolonged_bootstrap_period: Duration,
     pub force_bootstrap: bool,
+    pub ibd: IbdConfig<NodeId>,
+}
+
+/// IBD configuration.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct IbdConfig<NodeId>
+where
+    NodeId: Clone + Eq + Hash,
+{
+    /// Peers to download blocks from.
+    pub peers: HashSet<NodeId>,
+    /// The max number of blocks to download by a single request.
+    // TODO: This is not used anywhere yet. Add this to the download request
+    // by modifying related implemn
+    pub download_limit: NonZeroUsize,
+    /// The max number of downloads to repeat for a single peer.
+    pub max_download_iterations: NonZeroUsize,
 }
