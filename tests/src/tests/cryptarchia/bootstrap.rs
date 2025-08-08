7b4f0fbe72dddc68603e6ff5a1ddb375fee2b727
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-08-08 12:49:46 +0900

    improve tests

--- tests/src/tests/cryptarchia/bootstrap.rs
+++ tests/src/tests/cryptarchia/bootstrap.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, time::Duration};
+use std::{collections::HashSet, ops::Div as _, time::Duration};
 
 use futures::stream::{self, StreamExt as _};
 use nomos_libp2p::PeerId;
@@ -6,30 +6,32 @@ use tests::{
     adjust_timeout,
     nodes::validator::{create_validator_config, Validator},
     secret_key_to_peer_id,
-    topology::configs::create_general_configs,
+    topology::configs::{create_general_configs, GeneralConfig},
 };
 
 #[tokio::test]
 async fn test_ibd_behind_nodes() {
     let n_validators = 4;
+    let n_initial_validators = 2;
 
     let general_configs = create_general_configs(n_validators);
 
     let mut validators = vec![];
-    for config in general_configs.iter().take(2) {
+    for config in general_configs.iter().take(n_initial_validators) {
         let config = create_validator_config(config.clone());
         validators.push(Validator::spawn(config).await.unwrap());
     }
+    let block_time = calculate_block_time(general_configs.first().unwrap());
 
     println!("Testing IBD while initial validators are still bootstrapping...");
 
     let initial_peer_ids: HashSet<PeerId> = general_configs
         .iter()
-        .take(2)
+        .take(n_initial_validators)
         .map(|config| secret_key_to_peer_id(config.network_config.swarm_config.node_key.clone()))
         .collect();
 
-    let mut config = create_validator_config(general_configs[2].clone());
+    let mut config = create_validator_config(general_configs[n_initial_validators].clone());
     config.cryptarchia.bootstrap.ibd.peers = initial_peer_ids.clone();
 
     let failing_behind_node = Validator::spawn(config).await;
@@ -54,22 +56,50 @@ async fn test_ibd_behind_nodes() {
 
     println!("Behind node started, waiting for it to sync...");
 
-    // 1 second is enough to download the initial blocks and catch up
-    tokio::time::sleep(adjust_timeout(Duration::from_secs(1))).await;
+    // 1 second is enough to catch up to the initial validators
+    let conservative_ibd_duration = adjust_timeout(Duration::from_secs(1));
+    tokio::time::sleep(conservative_ibd_duration).await;
 
-    let initial_current_heights: Vec<_> = stream::iter(&validators)
+    // Check if the behind node has caught up to the highest initial validator.
+    //
+    // Keep in mind that any validator could have produced new blocks,
+    // and they have been not yet propagated to the network, before we perform this check.
+    // Even the behind node could have produced blocks as soon as IBD was completed.
+    // It means that the behind node could have the chain higher than others.
+    let max_initial_validator_height = *stream::iter(&validators)
+        .take(n_initial_validators)
         .then(|n| async move { n.consensus_info().await.height })
-        .collect()
-        .await;
-
-    let initial_current_min_height = initial_current_heights.iter().min().unwrap();
-
+        .collect::<Vec<_>>()
+        .await
+        .iter()
+        .max()
+        .unwrap();
     let behind_node_info = behind_node.consensus_info().await;
     println!("behind node info: {behind_node_info:?}");
+    let conservative_height_check_duration = Duration::from_secs(1);
 
-    // IDB duration + 1 second that we waited after IDB
-    // should allow creating at most 1 additional block by other validators
-    assert!(behind_node_info.height >= *initial_current_min_height - 1);
+    let time_buffer = conservative_ibd_duration + conservative_height_check_duration;
+    let height_margin = time_buffer.div_duration_f64(block_time).ceil() as u64;
+    println!("Checking if the behind node has caught up to the highest initial validator: height_margin:{height_margin}");
+    assert!(
+        behind_node_info
+            .height
+            .abs_diff(max_initial_validator_height)
+            <= height_margin
+    );
+}
+
+fn calculate_block_time(general_config: &GeneralConfig) -> Duration {
+    Duration::from_secs(
+        1f64.div(
+            general_config
+                .consensus_config
+                .ledger_config
+                .consensus_config
+                .active_slot_coeff,
+        )
+        .floor() as u64,
+    )
 }
 
 async fn wait_for_validators_mode_and_height(
