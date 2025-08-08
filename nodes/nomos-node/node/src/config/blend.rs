df4ad0bfd2a8258ce26b6dfcd5e2caee0388d1c5
Antonio Antonino <antonio@status.im>
2025-08-08 11:55:25 +0200

    Last changes

--- nodes/nomos-node/node/src/config/blend.rs
+++ nodes/nomos-node/node/src/config/blend.rs
@@ -16,6 +16,7 @@ impl BlendConfig {
         nomos_blend_service::edge::settings::BlendConfig {
             backend: nomos_blend_service::edge::backends::libp2p::Libp2pBlendBackendSettings {
                 node_key: self.0.backend.node_key.clone(),
+                // TODO: Allow for edge service settings to be included here.
                 max_dial_attempts_per_peer_per_message: 3
                     .try_into()
                     .expect("Max dial attempts per peer per message cannot be zero."),
