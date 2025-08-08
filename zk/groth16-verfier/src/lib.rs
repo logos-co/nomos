2a30c5c3f0fde00211cc2695ac117f02f4919153
danielSanchezQ <3danimanimal@gmail.com>
2025-08-08 12:47:55 +0000

    Proper exports in lib

---
+++ zk/groth16-verfier/src/lib.rs
@@ -0,0 +1,10 @@
+mod proof;
+mod protocol;
+mod verification_key;
+mod verifier;
+
+use ark_bn254::Bn254;
+pub use verifier::groth16_verify;
+
+pub type Groth16Proof = proof::Proof<Bn254>;
+pub type Groth16VerificationKey = verification_key::VerificationKey<Bn254>;
