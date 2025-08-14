da04de283f8059970d1266f9bb923572acde5dc5
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-08-14 22:46:56 +0900

    feat(blend): session transition: core

--- nomos-blend/network/src/core/with_core/error.rs
+++ nomos-blend/network/src/core/with_core/error.rs
@@ -1,6 +1,12 @@
+use libp2p::{swarm::ConnectionId, PeerId};
+
 #[derive(Debug)]
 pub enum Error {
     /// There were no peers to send a message to.
     NoPeers,
+    NoSessionForPeer {
+        peer_id: PeerId,
+        connection_id: ConnectionId,
+    },
     InvalidMessage,
 }
