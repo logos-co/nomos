02bcfbf5c94e62b26ee59de38bc0d978ac46f4c5
danielSanchezQ <3danimanimal@gmail.com>
2025-08-14 16:19:07 +0000

    Fix constants

--- zk/poseidon2/src/hasher.rs
+++ zk/poseidon2/src/hasher.rs
@@ -1,6 +1,8 @@
 use ark_bn254::Fr;
 use num_bigint::BigUint;
 
+use crate::{Poseidon2Bn254, Poseidon2Bn254Params, Poseidon2SpongeBn254};
+
 pub struct Poseidon2Hasher {
     state: [Fr; 3],
 }
@@ -9,13 +11,14 @@ impl Poseidon2Hasher {
     pub fn new() -> Self {
         let initial =
             BigUint::from(2u64).pow(64) + BigUint::from(256u64).pow(3) + BigUint::from(1u64);
-        let state = [Fr::from(initial), Fr::from(0u64), Fr::from(0u64)];
+        let state = [Fr::from(0u64), Fr::from(0u64), Fr::from(initial)];
         Self { state }
     }
 
     fn update_one(&mut self, input: &[u8]) {
         let input = Fr::from(BigUint::from_bytes_be(input));
-        self.state[0] = &self.state[0] + &input;
+        self.state[2] = &self.state[2] + &input;
+        Poseidon2Bn254::permute_mut::<Poseidon2Bn254Params, 3>(&mut self.state);
     }
 
     pub fn update(&mut self, input: &[u8]) {
@@ -25,13 +28,15 @@ impl Poseidon2Hasher {
     }
 
     pub fn finalize(&mut self) -> Fr {
-        self.state[0]
+        self.state[2]
     }
 }
 
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
+
+    use ark_ff::BigInteger;
 
     use super::*;
 
