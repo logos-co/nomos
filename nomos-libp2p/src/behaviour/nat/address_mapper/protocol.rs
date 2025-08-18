7cc774c2a5b629173df0b053912f23c6cd915fd1
Andrus Salumets <salumets.andrus@gmail.com>
2025-08-18 08:30:28 +0200

    feat(nat): nat-pmp

--- nomos-libp2p/src/behaviour/nat/address_mapper/protocol.rs
+++ nomos-libp2p/src/behaviour/nat/address_mapper/protocol.rs
@@ -1,42 +1,70 @@
 use libp2p::Multiaddr;
 
-use crate::behaviour::nat::address_mapper::{errors::AddressMapperError, upnp::UpnpProtocol};
+use crate::{
+    behaviour::nat::address_mapper::{
+        errors::AddressMapperError, nat_pmp::NatPmp, upnp::UpnpProtocol,
+    },
+    config::NatMappingSettings,
+};
 
 /// Trait for NAT mapping protocols
 #[async_trait::async_trait]
 pub trait MappingProtocol: Send + Sync + 'static {
     /// Initialize the protocol
-    async fn initialize() -> Result<Box<Self>, AddressMapperError>
+    async fn initialize(settings: NatMappingSettings) -> Result<Box<Self>, AddressMapperError>
     where
         Self: Sized;
 
-    /// Map a TCP address and return the external address
+    /// Tries to map an address and returns the external address
     async fn map_address(&mut self, address: &Multiaddr) -> Result<Multiaddr, AddressMapperError>;
 }
 
-pub struct ProtocolManager {
-    upnp: Box<dyn MappingProtocol>,
-}
+pub struct ProtocolManager;
 
 impl ProtocolManager {
-    pub async fn initialize() -> Result<Self, AddressMapperError> {
-        let upnp = UpnpProtocol::initialize().await?;
-        Ok(Self { upnp })
-    }
-
     pub async fn try_map_address(
-        &mut self,
+        settings: NatMappingSettings,
         address: &Multiaddr,
     ) -> Result<Multiaddr, AddressMapperError> {
-        let external_address = self.upnp.map_address(address).await?;
+        let mut nat_pmp = NatPmp::initialize(settings).await?;
+        if let Ok(external_address) = nat_pmp.map_address(address).await {
+            tracing::info!("Successfully mapped {address} to {external_address} using NAT-PMP");
 
-        tracing::info!(
-            "Successfully mapped {} to {} using {}",
-            address,
-            external_address,
-            "UPnP"
-        );
+            return Ok(external_address);
+        }
+
+        let mut upnp = UpnpProtocol::initialize(settings).await?;
+        let external_address = upnp.map_address(address).await?;
+        tracing::info!("Successfully mapped {address} to {external_address} using UPnP");
 
         Ok(external_address)
+    }
+}
+
+#[cfg(test)]
+mod real_gateway_tests {
+    use rand::{thread_rng, Rng as _};
+
+    use super::*;
+
+    /// Run with: NAT_TEST_LOCAL_IP="192.168.1.100" cargo test
+    /// map_address_via_protocol_manager_real_gateway -- --ignored
+    #[tokio::test]
+    #[ignore = "Runs against a real gateway"]
+    async fn map_address_via_protocol_manager_real_gateway() {
+        let local_ip = std::env::var("NAT_TEST_LOCAL_IP").expect("NAT_TEST_LOCAL_IP");
+        let random_port: u64 = thread_rng().gen_range(10000..=64000);
+        let internal_addr = format!("/ip4/{local_ip}/tcp/{random_port}");
+
+        println!("Testing NAT mapping for internal address: {internal_addr}",);
+
+        let internal_addr: Multiaddr = internal_addr.parse().expect("valid multiaddr");
+
+        let settings = NatMappingSettings::default();
+        let external = ProtocolManager::try_map_address(settings, &internal_addr)
+            .await
+            .expect("initialize ProtocolManager");
+
+        println!("Successfully mapped {internal_addr} to {external}");
     }
 }
---
+++ nomos-libp2p/src/config/nat/mod.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+pub mod autonat_client;
+pub mod mapping;
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, Default)]
+pub struct Settings {
+    pub autonat: autonat_client::Settings,
+    pub mapping: mapping::Settings,
+}
