08be70c2ca15309dd9a5073fcf939882c1399e62
danielSanchezQ <3danimanimal@gmail.com>
2025-08-21 18:20:31 +0000

    Start implementing public inputs generator method

--- zk/proofs/pol/src/proving_key/mod.rs
+++ zk/proofs/pol/src/proving_key/mod.rs
@@ -4,6 +4,12 @@ use crate::verification_key::PolVerifyingKey;
 
 pub struct PolProvingKey(Box<[u8]>);
 
+impl AsRef<[u8]> for PolProvingKey {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
+
 #[expect(clippy::large_include_file, reason = "Proving key is large")]
 static POL_PROVING_KEY: LazyLock<PolProvingKey> =
     LazyLock::new(|| PolProvingKey(include_bytes!("pol.zkey").to_vec().into_boxed_slice()));
