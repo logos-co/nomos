c0553e5afa55d0225b65deac5e34f5ca902fbd8f
danielSanchezQ <3danimanimal@gmail.com>
2025-08-21 18:20:31 +0000

    Start implementing public inputs generator method

--- zk/proofs/pol/src/public_inputs.rs
+++ zk/proofs/pol/src/public_inputs.rs
@@ -1,4 +1,11 @@
+use std::{
+    ops::{Add, Div},
+    sync::LazyLock,
+};
+
+use ark_ec::pairing::Pairing;
 use groth16::{Bn254, Fr, Groth16PublicInput};
+use primitive_types::U256;
 
 pub struct PolPublicInputs {
     slot_number: Groth16PublicInput,
@@ -13,10 +20,42 @@ pub struct PolPublicInputs {
 }
 
 pub struct PolPublicInputsData {
-    slot_number: usize,
-    epoch_nonce: usize,
-    total_stake: usize,
+    slot_number: u64,
+    epoch_nonce: u64,
+    total_stake: u64,
+    merkle_root: [u8; 32],
+    latest_root: [u8; 32],
+    leader_pk: ([u8; 16], [u8; 16]),
+    voucher_cml: [u8; 32],
+    entropy_contribution: [u8; 32],
 }
+
+static P: LazyLock<U256> = LazyLock::new(|| {
+    U256::from_str_radix(
+        "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001",
+        16,
+    )
+    .expect("P must be a valid hex string")
+});
+// t0 constant :
+// 0x27b6fe27507ca57ca369280400c79b5d2f58ff94d87cb0fbfc8294eb69eb1ea
+static T0_CONSTANT: LazyLock<U256> = LazyLock::new(|| {
+    U256::from_str_radix(
+        "0x27b6fe27507ca57ca369280400c79b5d2f58ff94d87cb0fbfc8294eb69eb1ea",
+        16,
+    )
+    .expect("Constant must be a valid hex string")
+});
+
+// t1 constant:
+// -0x104bfd09ebdd0a57772289d0973489b62662a4dc6f09da8b4af3c5cfb1dcdd
+static T1_CONSTANT: LazyLock<U256> = LazyLock::new(|| {
+    U256::from_str_radix(
+        "0x104bfd09ebdd0a57772289d0973489b62662a4dc6f09da8b4af3c5cfb1dcdd",
+        16,
+    )
+    .expect("Constant must be a valid hex string")
+});
 
 impl TryFrom<PolPublicInputsData> for PolPublicInputs {
     type Error = String;
@@ -26,8 +65,25 @@ impl TryFrom<PolPublicInputsData> for PolPublicInputs {
             slot_number,
             epoch_nonce,
             total_stake,
+            merkle_root,
+            latest_root,
+            leader_pk: (pk1, pk2),
+            voucher_cml,
+            entropy_contribution,
         }: PolPublicInputsData,
     ) -> Result<Self, Self::Error> {
+        let slot_number = U256::from(slot_number);
+        if slot_number > *P {
+            return Err("Slot number is greater than P".to_string());
+        }
+        let epoch_nonce = U256::from(epoch_nonce);
+        if epoch_nonce > *P {
+            return Err("Epoch nonce is greater than P".to_string());
+        }
+        let total_stake = U256::from(total_stake);
+
+        let lottery_0 = T0_CONSTANT.div(total_stake);
+        let lottery_1 = T1_CONSTANT.div(total_stake.pow(U256::from(2u8))).add(*P);
         todo!()
     }
 }
