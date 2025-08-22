4175776247fd39005c3a4ae6617a1bd9dbab8b5b
danielSanchezQ <3danimanimal@gmail.com>
2025-08-22 12:39:07 +0000

    Implement public inputs

--- zk/proofs/pol/src/public_inputs.rs
+++ zk/proofs/pol/src/public_inputs.rs
@@ -1,10 +1,13 @@
 use std::{
-    ops::{Add, Div},
+    ops::{Add, Div, Sub},
     sync::LazyLock,
 };
 
+use ark_bn254::Fq;
 use ark_ec::pairing::Pairing;
+use ark_ff::{BigInt, BigInteger};
 use groth16::{Bn254, Fr, Groth16PublicInput};
+use num_bigint::BigUint;
 use primitive_types::U256;
 
 pub struct PolPublicInputs {
@@ -12,22 +15,21 @@ pub struct PolPublicInputs {
     epoch_nonce: Groth16PublicInput,
     lottery_0: Groth16PublicInput,
     lottery_1: Groth16PublicInput,
-    merkle_root: Groth16PublicInput,
+    aged_root: Groth16PublicInput,
     latest_root: Groth16PublicInput,
-    leader_pk: Groth16PublicInput,
-    voucher_cml: Groth16PublicInput,
+    leader_pk_1: Groth16PublicInput,
+    leader_pk_2: Groth16PublicInput,
     entropy_contribution: Groth16PublicInput,
 }
 
 pub struct PolPublicInputsData {
+    entropy_contribution: [u8; 32],
     slot_number: u64,
     epoch_nonce: u64,
     total_stake: u64,
-    merkle_root: [u8; 32],
+    aged_root: [u8; 32],
     latest_root: [u8; 32],
     leader_pk: ([u8; 16], [u8; 16]),
-    voucher_cml: [u8; 32],
-    entropy_contribution: [u8; 32],
 }
 
 static P: LazyLock<U256> = LazyLock::new(|| {
@@ -62,14 +64,13 @@ impl TryFrom<PolPublicInputsData> for PolPublicInputs {
 
     fn try_from(
         PolPublicInputsData {
+            entropy_contribution,
             slot_number,
             epoch_nonce,
             total_stake,
-            merkle_root,
+            aged_root,
             latest_root,
             leader_pk: (pk1, pk2),
-            voucher_cml,
-            entropy_contribution,
         }: PolPublicInputsData,
     ) -> Result<Self, Self::Error> {
         let slot_number = U256::from(slot_number);
@@ -83,23 +84,48 @@ impl TryFrom<PolPublicInputsData> for PolPublicInputs {
         let total_stake = U256::from(total_stake);
 
         let lottery_0 = T0_CONSTANT.div(total_stake);
-        let lottery_1 = T1_CONSTANT.div(total_stake.pow(U256::from(2u8))).add(*P);
-        todo!()
+        let lottery_1 = P.sub(T1_CONSTANT.div(total_stake.pow(U256::from(2u8))));
+
+        Ok(Self {
+            slot_number: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(
+                slot_number.to_little_endian().as_ref(),
+            ))),
+            epoch_nonce: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(
+                epoch_nonce.to_little_endian().as_ref(),
+            ))),
+            lottery_0: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(
+                lottery_0.to_little_endian().as_ref(),
+            ))),
+            lottery_1: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(
+                lottery_1.to_little_endian().as_ref(),
+            ))),
+            aged_root: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(
+                aged_root.as_ref(),
+            ))),
+            latest_root: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(
+                latest_root.as_ref(),
+            ))),
+            leader_pk_1: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(pk1.as_ref()))),
+            leader_pk_2: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(pk2.as_ref()))),
+            entropy_contribution: Groth16PublicInput::new(Fr::from(BigUint::from_bytes_le(
+                entropy_contribution.as_ref(),
+            ))),
+        })
     }
 }
 
 impl PolPublicInputs {
     pub const fn to_inputs(&self) -> [Fr; 9] {
         [
+            self.entropy_contribution.into_inner(),
             self.slot_number.into_inner(),
             self.epoch_nonce.into_inner(),
             self.lottery_0.into_inner(),
             self.lottery_1.into_inner(),
-            self.merkle_root.into_inner(),
+            self.aged_root.into_inner(),
             self.latest_root.into_inner(),
-            self.leader_pk.into_inner(),
-            self.voucher_cml.into_inner(),
-            self.entropy_contribution.into_inner(),
+            self.leader_pk_1.into_inner(),
+            self.leader_pk_2.into_inner(),
         ]
     }
 }
