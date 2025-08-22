ed98e4c27236692c663b4ec8c5670165b1883ce1
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-08-22 11:36:09 +0900

    with_core test

---
+++ nomos-blend/network/src/core/with_core/behaviour/tests/session.rs
@@ -0,0 +1,191 @@
+use std::time::Duration;
+
+use futures::StreamExt as _;
+use libp2p::{Multiaddr, PeerId};
+use libp2p_swarm_test::SwarmExt as _;
+use nomos_blend_message::crypto::Ed25519PrivateKey;
+use nomos_blend_scheduling::membership::{Membership, Node};
+use nomos_libp2p::SwarmEvent;
+use test_log::test;
+use tokio::{select, time::sleep};
+
+use crate::core::{
+    tests::utils::{TestEncapsulatedMessage, TestSwarm},
+    with_core::{
+        behaviour::{
+            tests::utils::{BehaviourBuilder, SwarmExt as _},
+            Event,
+        },
+        error::Error,
+    },
+};
+
+#[test(tokio::test)]
+async fn publish_message_after_new_session_started() {
+    let mut dialer = TestSwarm::new(|id| BehaviourBuilder::default().with_identity(id).build());
+    let mut listener = TestSwarm::new(|id| BehaviourBuilder::default().with_identity(id).build());
+
+    listener.listen().with_memory_addr_external().await;
+    dialer
+        .connect_and_wait_for_outbound_upgrade(&mut listener)
+        .await;
+
+    // Start a new session.
+    let memberships = build_memberships(&[*dialer.local_peer_id(), *listener.local_peer_id()]);
+    dialer
+        .behaviour_mut()
+        .start_new_session(memberships[0].clone());
+    listener
+        .behaviour_mut()
+        .start_new_session(memberships[1].clone());
+
+    // Send a message but expect [`Error::NoPeers`]
+    // because we haven't establish connections for the new session.
+    let test_message = TestEncapsulatedMessage::new(b"msg");
+    let result = dialer
+        .behaviour_mut()
+        .validate_and_publish_message(test_message.clone());
+    assert_eq!(result, Err(Error::NoPeers));
+
+    // Establish a connection for the new session.
+    dialer
+        .connect_and_wait_for_outbound_upgrade(&mut listener)
+        .await;
+
+    // Now we can send the message successfully.
+    dialer
+        .behaviour_mut()
+        .validate_and_publish_message(test_message.clone())
+        .unwrap();
+    loop {
+        select! {
+            _ = dialer.select_next_some() => {}
+            event = listener.select_next_some() => {
+                if let SwarmEvent::Behaviour(Event::Message(message, _)) = event {
+                    assert_eq!(message.id(), test_message.id());
+                    break;
+                }
+            }
+        }
+    }
+}
+
+#[test(tokio::test)]
+async fn forward_message_in_old_session() {
+    let mut sender = TestSwarm::new(|id| BehaviourBuilder::default().with_identity(id).build());
+    let mut forwarder = TestSwarm::new(|id| {
+        BehaviourBuilder::default()
+            .with_identity(id)
+            .with_peering_degree(2..=2)
+            .build()
+    });
+    let mut receiver = TestSwarm::new(|id| BehaviourBuilder::default().with_identity(id).build());
+
+    forwarder.listen().with_memory_addr_external().await;
+    receiver.listen().with_memory_addr_external().await;
+
+    // Connect nodes: Sender -> Forwarder -> Receiver
+    sender
+        .connect_and_wait_for_outbound_upgrade(&mut forwarder)
+        .await;
+    forwarder
+        .connect_and_wait_for_outbound_upgrade(&mut receiver)
+        .await;
+
+    // Before sending any message, start a new session only for the forwarder and receiver.
+    // Then, the topology looks like:
+    // - Old session: Sender -> Forwarder -> Receiver
+    // - New session: No connections yet
+    let memberships = build_memberships(&[
+        *sender.local_peer_id(),
+        *forwarder.local_peer_id(),
+        *receiver.local_peer_id(),
+    ]);
+    forwarder
+        .behaviour_mut()
+        .start_new_session(memberships[1].clone());
+    receiver
+        .behaviour_mut()
+        .start_new_session(memberships[2].clone());
+
+    // The sender publishes a message to the forwarder.
+    // Publishing messages always uses the current session.
+    // That's why we didn't start a new session for the sender yet.
+    let test_message = TestEncapsulatedMessage::new(b"msg");
+    sender
+        .behaviour_mut()
+        .validate_and_publish_message(test_message.clone())
+        .unwrap();
+    // We expect that the message goes through the forwarder and receiver.
+    loop {
+        select! {
+            _ = sender.select_next_some() => {}
+            event = forwarder.select_next_some() => {
+                if let SwarmEvent::Behaviour(Event::Message(message, conn)) = event {
+                    assert_eq!(message.id(), test_message.id());
+                    forwarder.behaviour_mut()
+                        .forward_validated_message(&message, conn)
+                        .unwrap();
+                }
+            }
+            event = receiver.select_next_some() => {
+                if let SwarmEvent::Behaviour(Event::Message(message, _)) = event {
+                    assert_eq!(message.id(), test_message.id());
+                    break;
+                }
+            }
+        }
+    }
+}
+
+#[test(tokio::test)]
+async fn close_old_session() {
+    let mut dialer = TestSwarm::new(|id| BehaviourBuilder::default().with_identity(id).build());
+    let mut listener = TestSwarm::new(|id| BehaviourBuilder::default().with_identity(id).build());
+
+    listener.listen().with_memory_addr_external().await;
+    dialer
+        .connect_and_wait_for_outbound_upgrade(&mut listener)
+        .await;
+
+    // Start a new session.
+    let memberships = build_memberships(&[*dialer.local_peer_id(), *listener.local_peer_id()]);
+    dialer
+        .behaviour_mut()
+        .start_new_session(memberships[0].clone());
+    listener
+        .behaviour_mut()
+        .start_new_session(memberships[1].clone());
+
+    // Finish the transition period
+    dialer.behaviour_mut().close_old_session();
+    listener.behaviour_mut().close_old_session();
+
+    // Expect that the connection is closed after 10s (default swarm timeout).
+    loop {
+        select! {
+            _ = dialer.select_next_some() => {}
+            event = listener.select_next_some() => {
+                if let SwarmEvent::ConnectionClosed { .. } = event {
+                    assert!(listener.behaviour_mut().old_session.is_none());
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn build_memberships(ids: &[PeerId]) -> Vec<Membership<PeerId>> {
+    let nodes = ids
+        .iter()
+        .map(|id| Node {
+            id: *id,
+            address: Multiaddr::empty(),
+            public_key: Ed25519PrivateKey::generate().public_key(),
+        })
+        .collect::<Vec<_>>();
+    nodes
+        .iter()
+        .map(|node| Membership::new(&nodes, Some(&node.public_key)))
+        .collect()
+}
