c0f717c1d918e18c35811d0e8eb6e501dcd581ce
danielSanchezQ <3danimanimal@gmail.com>
2025-08-22 14:58:32 +0000

    Implement all inputs types massaging

---
+++ zk/proofs/pol/src/inputs.rs
@@ -0,0 +1,39 @@
+use serde::Serialize;
+
+use crate::{
+    private_inputs::{PolPrivateInputs, PolPrivateInputsJson},
+    public_inputs::{PolPublicInputs, PolPublicInputsJson},
+};
+
+#[derive(Clone, Serialize)]
+#[serde(into = "PolInputsJson", rename_all = "snake_case")]
+pub struct PolInputs {
+    pub private: PolPrivateInputs,
+    pub public: PolPublicInputs,
+}
+
+impl PolInputs {
+    pub const fn from_public_and_private(
+        public: PolPublicInputs,
+        private: PolPrivateInputs,
+    ) -> Self {
+        Self { private, public }
+    }
+}
+
+#[derive(Serialize)]
+struct PolInputsJson {
+    #[serde(flatten)]
+    pub private: PolPrivateInputsJson,
+    #[serde(flatten)]
+    pub public: PolPublicInputsJson,
+}
+
+impl From<PolInputs> for PolInputsJson {
+    fn from(inputs: PolInputs) -> Self {
+        Self {
+            private: (&inputs.private).into(),
+            public: (&inputs.public).into(),
+        }
+    }
+}
