dfa9496de79271564410b4e9711b5d4ee1b53aca
danielSanchezQ <3danimanimal@gmail.com>
2025-08-22 15:46:09 +0000

    Implement pol verify

--- zk/proofs/pol/src/lib.rs
+++ zk/proofs/pol/src/lib.rs
@@ -4,9 +4,16 @@ mod proving_key;
 mod public_inputs;
 mod verification_key;
 
+use std::error::Error;
+
 pub use inputs::PolInputs;
 pub use private_inputs::{PolPrivateInputs, PolPrivateInputsData};
 pub use proving_key::PolProvingKey;
 pub use public_inputs::{PolPublicInputs, PolPublicInputsData};
 pub use verification_key::PolVerifyingKey;
 pub type PoLProof = groth16::Groth16Proof;
+
+pub fn verify(proof: &PoLProof, public_inputs: &PolPublicInputs) -> Result<bool, impl Error> {
+    let inputs = public_inputs.to_inputs();
+    groth16::groth16_verify(verification_key::POL_VK.as_ref(), proof, &inputs)
+}
