b30a8d35c527d3ff2187b9599a106557e58ccdb2
Daniel <sanchez.quiros.daniel@gmail.com>
2025-08-28 12:47:21 +0200

    Add serde test

--- zk/groth16/src/serde.rs
+++ zk/groth16/src/serde.rs
@@ -1,6 +1,6 @@
 pub mod serde_fr {
     use ark_bn254::Fr;
-    use ark_ff::biginteger::BigInteger as _;
+    use ark_ff::{PrimeField as _, biginteger::BigInteger as _};
     use num_bigint::BigUint;
     use serde::{Deserialize as _, Deserializer, Serializer};
 
@@ -8,7 +8,7 @@ pub mod serde_fr {
     where
         S: Serializer,
     {
-        let hex = hex::encode(item.0.to_bytes_le()); // Convert `Fr` to hex representation
+        let hex = hex::encode(item.into_bigint().to_bytes_le()); // Convert `Fr` to hex representation
         serializer.serialize_str(&hex)
     }
 
@@ -19,5 +19,22 @@ pub mod serde_fr {
         let hex_str = String::deserialize(deserializer)?;
         let bytes = hex::decode(hex_str).map_err(serde::de::Error::custom)?;
         Ok(BigUint::from_bytes_le(&bytes).into()) // Parse from hex
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use ark_bn254::Fr;
+        use num_bigint::BigUint;
+        use serde::{Deserialize, Serialize};
+
+        #[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
+        struct FrDeser(#[serde(with = "crate::serde::serde_fr")] pub Fr);
+        #[test]
+        fn test_serialize_deserialize() {
+            let fr1 = FrDeser(BigUint::from(123u8).into());
+            let v = serde_json::to_string(&fr1).unwrap();
+            let fr2: FrDeser = serde_json::from_str(&v).unwrap();
+            assert_eq!(fr1, fr2);
+        }
     }
 }
