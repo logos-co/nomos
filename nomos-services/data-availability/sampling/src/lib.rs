1153c84c935d0989e66f04020b683773799626b3
Petar Radovic <petar.radovic@gmail.com>
2025-09-02 19:23:50 +0200

    add comment about spawn blocking

--- nomos-services/data-availability/sampling/src/lib.rs
+++ nomos-services/data-availability/sampling/src/lib.rs
@@ -458,7 +458,8 @@ where
                                     return false;
                                 }
 
-                                // Verify all shares
+                                // todo: maybe spawn blocking so it yields why it verifies on a
+                                // separate thread Verify all shares
                                 for (blob_id, blob_shares) in &shares {
                                     if let Some(blob_commitments) = commitments.get(blob_id) {
                                         for share in blob_shares {
