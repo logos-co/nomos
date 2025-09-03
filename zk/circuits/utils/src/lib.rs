9870c85f405bd6978e1cc848456e7c6ebaffcbea
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-09-03 12:53:30 +0200

    Generalise binary mentions to file.

--- zk/circuits/utils/src/lib.rs
+++ zk/circuits/utils/src/lib.rs
@@ -5,32 +5,32 @@ const CRATE_BIN_PATH: &str = "bin";
 const PATH_ENV_VAR: &str = "PATH";
 const PATH_SEPARATOR: &str = ":";
 
-/// Check if a binary exists in the crate's bin directory
+/// Find a file in the crate's bin directory
 ///
 /// # Arguments
 ///
-/// * `binary_name` - The name of the binary to check.
+/// * `file_name` - The name of the file to find.
 #[must_use]
-pub fn find_binary_in_crate(binary_name: &str) -> Option<PathBuf> {
+pub fn find_file_in_crate(file_name: &str) -> Option<PathBuf> {
     let path = std::env::var(CARGO_MANIFEST_DIR).ok().map(|directory| {
         PathBuf::from(directory)
             .join(CRATE_BIN_PATH)
-            .join(binary_name)
+            .join(file_name)
     })?;
 
     path.is_file().then_some(path)
 }
 
-/// Check if a binary exists in an environment variable
+/// Find a file in an environment variable
 ///
 /// # Arguments
 ///
-/// * `binary_name` - The name of the binary to check.
+/// * `file_name` - The name of the file to check.
 /// * `environment_variable` - The name of the environment variable that may
-///   contain the binary directory or path.
+///   contain the file directory or path.
 #[must_use]
-pub fn find_binary_in_environment_variable(
-    binary_name: &str,
+pub fn find_file_in_environment_variable(
+    file_name: &str,
     environment_variable: &str,
 ) -> Option<PathBuf> {
     let path = std::env::var(environment_variable)
@@ -41,46 +41,47 @@ pub fn find_binary_in_environment_variable(
         return Some(path);
     }
 
-    let binary_path = path.join(binary_name);
-    binary_path.is_file().then_some(binary_path)
+    let file_path = path.join(file_name);
+    file_path.is_file().then_some(file_path)
 }
 
-/// Check if a binary exists in the system PATH
+/// Find a file in the system PATH
 ///
 /// # Arguments
 ///
-/// * `binary_name` - The name of the binary to check.
+/// * `file_name` - The name of the file to check.
 #[must_use]
-pub fn find_binary_in_path(binary_name: &str) -> Option<PathBuf> {
+pub fn find_file_in_path(file_name: &str) -> Option<PathBuf> {
     std::env::var(PATH_ENV_VAR)
         .ok()?
         .split(PATH_SEPARATOR)
-        .map(|dir| PathBuf::from(dir).join(binary_name))
+        .map(|dir| PathBuf::from(dir).join(file_name))
         .find(|path| path.is_file())
 }
 
-/// Find the binary
+/// Find a file by checking multiple locations.
+///
 /// This function checks the repository's bin directory, an environment
 /// variable, and finally falls back to the system PATH.
 ///
 /// # Arguments
 ///
-/// * `binary_name` - The name of the binary to find.
+/// * `file_name` - The name of the file to find.
 /// * `environment_variable` - The name of the environment variable that may
-///   contain the binary directory or path.
+///   contain the file directory or path.
 ///
 /// # Returns
 ///
-/// An `Option<PathBuf>` that contains the path to the binary if found, or
+/// An `Option<PathBuf>` that contains the path to the file if found, or
 /// `None` if not found.
-pub fn find_binary(binary_name: &str, environment_variable: &str) -> Result<PathBuf, String> {
-    let binary = find_binary_in_crate(binary_name)
-        .or_else(|| find_binary_in_environment_variable(binary_name, environment_variable))
-        .or_else(|| find_binary_in_path(binary_name));
+pub fn find_file(file_name: &str, environment_variable: &str) -> Result<PathBuf, String> {
+    let file = find_file_in_crate(file_name)
+        .or_else(|| find_file_in_environment_variable(file_name, environment_variable))
+        .or_else(|| find_file_in_path(file_name));
 
-    binary.ok_or_else(||
+    file.ok_or_else(||
         format!(
-            "Binary '{binary_name}' not found in the crate-relative 'bin/' directory, environment variable ({environment_variable}), or system PATH.",
+            "File '{file_name}' could not be found in the crate-relative 'bin/' directory, environment variable ({environment_variable}), or system PATH.",
         )
     )
 }
