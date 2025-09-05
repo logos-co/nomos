c26ffdd88cfac05a00a07a7031f970f791e102dc
Gusto <bacvinka@gmail.com>
2025-09-05 17:34:22 +0300

    Use note id from tests in wallet mock

--- nomos-services/data-availability/dispersal/src/adapters/wallet/mock.rs
+++ nomos-services/data-availability/dispersal/src/adapters/wallet/mock.rs
@@ -5,20 +5,25 @@ use nomos_core::{
     mantle::{
         ledger::Tx as LedgerTx,
         ops::channel::{blob::BlobOp, ChannelId, Ed25519PublicKey, MsgId},
-        MantleTx, Op, SignedMantleTx, Transaction as _,
+        MantleTx, Note, Op, SignedMantleTx, Transaction as _, Utxo,
     },
     proofs::zksig::{DummyZkSignature, ZkSignaturePublic},
 };
+use num_bigint::BigUint;
 
 use super::DaWalletAdapter;
 
-pub struct MockWalletAdapter;
+pub struct MockWalletAdapter {
+    last_msg_id: MsgId,
+}
 
 impl DaWalletAdapter for MockWalletAdapter {
     type Error = Infallible;
 
     fn new() -> Self {
-        Self
+        Self {
+            last_msg_id: MsgId::root(),
+        }
     }
 
     fn blob_tx(
@@ -27,20 +32,30 @@ impl DaWalletAdapter for MockWalletAdapter {
         blob_size: usize,
         signer: Ed25519PublicKey,
     ) -> Result<SignedMantleTx, Self::Error> {
+        // TODO: This mock implementation targets to only work with integration tests.
+        // When integration tests genesis_state changes, this part should be updated, or
+        // removed all together after an actual wallet service can create signed mantle
+        // transaction with blob operation.
+        let utxo = Utxo {
+            note: Note::new(1, BigUint::from(0u8).into()),
+            tx_hash: BigUint::from(0u8).into(),
+            output_index: 0,
+        };
+
         let blob_op = BlobOp {
             channel: ChannelId::from([0; 32]),
             blob,
             blob_size: blob_size as u64,
-            da_storage_gas_price: 0,
-            parent: MsgId::root(),
+            da_storage_gas_price: 3000,
+            parent: self.last_msg_id,
             signer,
         };
 
         let mantle_tx = MantleTx {
             ops: vec![Op::ChannelBlob(blob_op)],
-            ledger_tx: LedgerTx::new(vec![], vec![]),
-            storage_gas_price: 0,
-            execution_gas_price: 0,
+            ledger_tx: LedgerTx::new(vec![utxo.id()], vec![]),
+            storage_gas_price: 3000,
+            execution_gas_price: 3000,
         };
 
         Ok(SignedMantleTx {
