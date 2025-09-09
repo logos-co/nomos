4e8a6c26cdf817f94525f25bad3c6c86b30ed9d8
danielSanchezQ <3danimanimal@gmail.com>
2025-09-09 11:21:22 +0000

    Implement non human redable serialization deserialization for Fr

--- zk/groth16/src/serde.rs
+++ zk/groth16/src/serde.rs
@@ -8,17 +8,27 @@ pub mod serde_fr {
     where
         S: Serializer,
     {
-        let hex = hex::encode(item.into_bigint().to_bytes_le()); // Convert `Fr` to hex representation
-        serializer.serialize_str(&hex)
+        let bytes = item.into_bigint().to_bytes_le();
+        if serializer.is_human_readable() {
+            let hex = hex::encode(bytes); // Convert `Fr` to hex representation
+            serializer.serialize_str(&hex)
+        } else {
+            serializer.serialize_bytes(&bytes)
+        }
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Fr, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let hex_str = String::deserialize(deserializer)?;
-        let bytes = hex::decode(hex_str).map_err(serde::de::Error::custom)?;
-        Ok(BigUint::from_bytes_le(&bytes).into()) // Parse from hex
+        if deserializer.is_human_readable() {
+            let hex_str = String::deserialize(deserializer)?;
+            let bytes = hex::decode(hex_str).map_err(serde::de::Error::custom)?;
+            Ok(BigUint::from_bytes_le(&bytes).into()) // Parse from hex    
+        } else {
+            let bytes = Vec::<u8>::deserialize(deserializer)?;
+            Ok(BigUint::from_bytes_le(&bytes).into())
+        }
     }
 
     #[cfg(test)]
@@ -30,10 +40,18 @@ pub mod serde_fr {
         #[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
         struct FrDeser(#[serde(with = "crate::serde::serde_fr")] pub Fr);
         #[test]
-        fn test_serialize_deserialize() {
+        fn test_serialize_deserialize_json() {
             let fr1 = FrDeser(BigUint::from(123u8).into());
             let v = serde_json::to_string(&fr1).unwrap();
             let fr2: FrDeser = serde_json::from_str(&v).unwrap();
+            assert_eq!(fr1, fr2);
+        }
+
+        #[test]
+        fn test_serialize_deserialize_bin() {
+            let fr1 = FrDeser(BigUint::from(123u8).into());
+            let v = bincode::serialize(&fr1).unwrap();
+            let fr2: FrDeser = bincode::deserialize(&v).unwrap();
             assert_eq!(fr1, fr2);
         }
     }
