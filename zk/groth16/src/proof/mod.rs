8022d203efb851d6ec15b62d06d0ef1fe6853649
Giacomo Pasini <g.pasini98@gmail.com>
2025-09-10 17:02:31 +0200

    Add debug and clone

--- zk/groth16/src/proof/mod.rs
+++ zk/groth16/src/proof/mod.rs
@@ -24,6 +24,7 @@ pub struct Proof<E: Pairing> {
     pub pi_c: E::G1Affine,
 }
 
+#[derive(Clone, Debug)]
 pub struct CompressedProof<
     const G1_COMPRESSED_SIZE: usize,
     const G2_COMPRESSED_SIZE: usize,
