5bef071246acf35ce00b8a584918957c0697d021
Antonio Antonino <antonio@status.im>
2025-09-10 16:47:10 +0200

    Verifier trait everywhere where needed

--- nomos-blend/message/src/crypto/proofs/mod.rs
+++ nomos-blend/message/src/crypto/proofs/mod.rs
@@ -1,2 +1,25 @@
+use groth16::Fr;
+
+use crate::crypto::proofs::quota::{inputs::prove::PublicInputs, ProofOfQuota};
+
 pub mod quota;
 pub mod selection;
+
+pub enum Error {
+    ProofOfQuota(quota::Error),
+}
+
+pub struct ProofsVerifier;
+
+impl crate::encap::ProofsVerifier for ProofsVerifier {
+    type Error = Error;
+    type ProofOfQuotaVerificationOutput = Fr;
+
+    fn verify_proof_of_quota(
+        &self,
+        proof: ProofOfQuota,
+        inputs: &PublicInputs,
+    ) -> Result<Self::ProofOfQuotaVerificationOutput, Self::Error> {
+        proof.verify(inputs).map_err(Error::ProofOfQuota)
+    }
+}
