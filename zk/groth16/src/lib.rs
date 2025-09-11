84f637a4faed262af2778655f302cacd8afb9c28
danielSanchezQ <3danimanimal@gmail.com>
2025-09-11 09:35:06 +0000

    Catch modulus error

--- zk/groth16/src/lib.rs
+++ zk/groth16/src/lib.rs
@@ -13,9 +13,11 @@ pub(crate) mod utils;
 mod verification_key;
 mod verifier;
 
+use std::error::Error;
+
 pub use ark_bn254::{Bn254, Fr};
 pub use ark_ff::Field;
-use ark_ff::{BigInteger as _, PrimeField as _};
+use ark_ff::{BigInteger as _, PrimeField};
 use num_bigint::BigUint;
 pub use verifier::groth16_verify;
 
@@ -44,9 +46,22 @@ pub fn fr_to_bytes(fr: Fr) -> FrBytes {
         .expect("Bn254 Fr to bytes should fit in 32 bytes")
 }
 
-#[must_use]
-pub fn fr_from_bytes(fr: &FrBytes) -> Fr {
-    BigUint::from_bytes_le(fr).into()
+#[derive(Debug, thiserror::Error)]
+#[error("Parsed bytes are bigger than the modulus, got {parsed_bytes}, expected {modulus}")]
+pub struct FrFromBytesError {
+    pub parsed_bytes: String,
+    pub modulus: String,
+}
+
+pub fn fr_from_bytes(fr: &FrBytes) -> Result<Fr, impl Error> {
+    let n = BigUint::from_bytes_le(fr);
+    if n > <Fr as PrimeField>::MODULUS.into() {
+        return Err(FrFromBytesError {
+            parsed_bytes: n.to_string(),
+            modulus: <Fr as PrimeField>::MODULUS.to_string(),
+        });
+    }
+    Ok(n.into())
 }
 
 #[cfg(test)]
@@ -60,7 +75,7 @@ mod tests {
     fn fr_to_from_bytes() {
         let value: Fr = BigUint::from(1_234_567_890_123_456_789u64).into();
         let bytes = fr_to_bytes(value);
-        let value2 = fr_from_bytes(&bytes);
+        let value2 = fr_from_bytes(&bytes).unwrap();
         assert_eq!(value, value2);
     }
 }
