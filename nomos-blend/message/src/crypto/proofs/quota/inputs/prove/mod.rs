fcec8e4f569c9d5d1bf1bfdd531e34a988377e46
Antonio Antonino <antonio@status.im>
2025-09-16 12:22:03 +0200

    Address Thomas feedback

--- nomos-blend/message/src/crypto/proofs/quota/inputs/prove/mod.rs
+++ nomos-blend/message/src/crypto/proofs/quota/inputs/prove/mod.rs
@@ -1,4 +1,3 @@
-use groth16::{Field as _, Fr};
 use num_bigint::BigUint;
 use poq::{
     PoQBlendInputsData, PoQChainInputsData, PoQCommonInputsData, PoQInputsFromDataError,
@@ -26,58 +25,53 @@ impl TryFrom<Inputs> for PoQWitnessInputs {
     fn try_from(value: Inputs) -> Result<Self, Self::Error> {
         let (signing_key_first_half, signing_key_second_half) =
             split_ephemeral_signing_key(value.public.signing_key);
-        let (blend_input_data, wallet_input_data) =
-            inputs_data_for_proof_type(value.private.proof_type);
-        Ok(Self {
-            chain: PoQChainInputsData {
-                core_root: value.public.core_root,
-                pol_epoch_nonce: value.public.pol_epoch_nonce,
-                pol_ledger_aged: value.public.pol_ledger_aged,
-                session: value.public.session,
-                total_stake: value.public.total_stake,
-            }
-            .try_into()?,
-            common: PoQCommonInputsData {
-                core_quota: value.public.core_quota,
-                index: value.private.key_index,
-                leader_quota: value.public.leader_quota,
-                message_key: (
-                    BigUint::from_bytes_le(&signing_key_first_half[..]).into(),
-                    BigUint::from_bytes_le(&signing_key_second_half[..]).into(),
-                ),
-                selector: value.private.selector,
-            }
-            .into(),
-            blend: blend_input_data.into(),
-            wallet: wallet_input_data.into(),
-        })
+        let chain_input_data = PoQChainInputsData {
+            core_root: value.public.core_root,
+            pol_epoch_nonce: value.public.pol_epoch_nonce,
+            pol_ledger_aged: value.public.pol_ledger_aged,
+            session: value.public.session,
+            total_stake: value.public.total_stake,
+        };
+        let common_input_data = PoQCommonInputsData {
+            core_quota: value.public.core_quota,
+            index: value.private.key_index,
+            leader_quota: value.public.leader_quota,
+            message_key: (
+                BigUint::from_bytes_le(&signing_key_first_half[..]).into(),
+                BigUint::from_bytes_le(&signing_key_second_half[..]).into(),
+            ),
+            selector: value.private.selector,
+        };
+        witness_input_for_proof_type(
+            chain_input_data,
+            common_input_data,
+            value.private.proof_type,
+        )
     }
 }
 
-fn inputs_data_for_proof_type(proof_type: ProofType) -> (PoQBlendInputsData, PoQWalletInputsData) {
+fn witness_input_for_proof_type(
+    chain_input_data: PoQChainInputsData,
+    common_input_data: PoQCommonInputsData,
+    proof_type: ProofType,
+) -> Result<PoQWitnessInputs, PoQInputsFromDataError> {
     match proof_type {
         ProofType::CoreQuota(ProofOfCoreQuotaInputs {
             core_path,
             core_path_selectors,
             core_sk,
-        }) => (
-            PoQBlendInputsData {
+        }) => {
+            let blend_input_data = PoQBlendInputsData {
                 core_path,
                 core_path_selectors,
                 core_sk,
-            },
-            PoQWalletInputsData {
-                aged_path: vec![Fr::ZERO; 32],
-                aged_selector: vec![false; 32],
-                note_value: u64::MIN,
-                output_number: u64::MIN,
-                slot: u64::MIN,
-                slot_secret: Fr::ZERO,
-                slot_secret_path: vec![Fr::ZERO; 25],
-                starting_slot: u64::MIN,
-                transaction_hash: Fr::ZERO,
-            },
-        ),
+            };
+            PoQWitnessInputs::from_core_node_data(
+                chain_input_data,
+                common_input_data,
+                blend_input_data,
+            )
+        }
         ProofType::LeadershipQuota(ProofOfLeadershipQuotaInputs {
             aged_path,
             aged_selector,
@@ -88,13 +82,8 @@ fn inputs_data_for_proof_type(proof_type: ProofType) -> (PoQBlendInputsData, PoQ
             slot_secret_path,
             starting_slot,
             transaction_hash,
-        }) => (
-            PoQBlendInputsData {
-                core_path: vec![Fr::ZERO; 20],
-                core_path_selectors: vec![false; 20],
-                core_sk: Fr::ZERO,
-            },
-            PoQWalletInputsData {
+        }) => {
+            let wallet_input_data = PoQWalletInputsData {
                 aged_path,
                 aged_selector,
                 note_value,
@@ -104,7 +93,12 @@ fn inputs_data_for_proof_type(proof_type: ProofType) -> (PoQBlendInputsData, PoQ
                 slot_secret_path,
                 starting_slot,
                 transaction_hash,
-            },
-        ),
+            };
+            PoQWitnessInputs::from_leader_data(
+                chain_input_data,
+                common_input_data,
+                wallet_input_data,
+            )
+        }
     }
 }
