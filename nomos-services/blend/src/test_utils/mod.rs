bf559ad5bde934e9a7e2fd462176445f01d78293
Antonio Antonino <antonio@status.im>
2025-10-09 12:05:40 +0300

    Checkpoint

--- nomos-services/blend/src/test_utils/mod.rs
+++ nomos-services/blend/src/test_utils/mod.rs
@@ -1,8 +1,8 @@
-pub mod crypto;
+// pub mod crypto;
 pub mod epoch;
-pub mod membership;
+// pub mod membership;
 
-#[cfg(feature = "libp2p")]
-mod libp2p;
-#[cfg(feature = "libp2p")]
-pub use self::libp2p::*;
+// #[cfg(feature = "libp2p")]
+// mod libp2p;
+// #[cfg(feature = "libp2p")]
+// pub use self::libp2p::*;
