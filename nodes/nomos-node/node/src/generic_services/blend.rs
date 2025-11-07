f1517ad3b6b4ada940697d0afda2334c99de7a7a
Antonio <antonio@status.im>
2025-11-07 08:43:23 +0100

    feat(blend): optimize message decapsulation (#1891)

--- nodes/nomos-node/node/src/generic_services/blend.rs
+++ nodes/nomos-node/node/src/generic_services/blend.rs
@@ -79,25 +79,12 @@ impl ProofsVerifier for BlendProofsVerifier {
     }
 }
 
-pub struct BlendProofsGenerator {
-    membership_size: usize,
-    local_node_index: Option<usize>,
-}
+pub struct BlendProofsGenerator;
 
 #[async_trait]
 impl CoreAndLeaderProofsGenerator for BlendProofsGenerator {
-    fn new(
-        ProofsGeneratorSettings {
-            local_node_index,
-            membership_size,
-            ..
-        }: ProofsGeneratorSettings,
-        _private_inputs: ProofOfCoreQuotaInputs,
-    ) -> Self {
-        Self {
-            membership_size,
-            local_node_index,
-        }
+    fn new(_settings: ProofsGeneratorSettings, _private_inputs: ProofOfCoreQuotaInputs) -> Self {
+        Self
     }
 
     fn rotate_epoch(&mut self, _new_epoch_public: LeaderInputs) {}
@@ -105,34 +92,21 @@ impl CoreAndLeaderProofsGenerator for BlendProofsGenerator {
     fn set_epoch_private(&mut self, _new_epoch_private: ProofOfLeadershipQuotaInputs) {}
 
     async fn get_next_core_proof(&mut self) -> Option<BlendLayerProof> {
-        Some(loop_until_valid_proof(
-            self.membership_size,
-            self.local_node_index,
-        ))
+        Some(loop_until_valid_proof())
     }
 
     async fn get_next_leader_proof(&mut self) -> Option<BlendLayerProof> {
-        Some(loop_until_valid_proof(
-            self.membership_size,
-            self.local_node_index,
-        ))
+        Some(loop_until_valid_proof())
     }
 }
 
 #[async_trait]
 impl LeaderProofsGenerator for BlendProofsGenerator {
     fn new(
-        ProofsGeneratorSettings {
-            local_node_index,
-            membership_size,
-            ..
-        }: ProofsGeneratorSettings,
+        _settings: ProofsGeneratorSettings,
         _private_inputs: ProofOfLeadershipQuotaInputs,
     ) -> Self {
-        Self {
-            membership_size,
-            local_node_index,
-        }
+        Self
     }
 
     fn rotate_epoch(
@@ -143,19 +117,11 @@ impl LeaderProofsGenerator for BlendProofsGenerator {
     }
 
     async fn get_next_proof(&mut self) -> BlendLayerProof {
-        loop_until_valid_proof(self.membership_size, self.local_node_index)
+        loop_until_valid_proof()
     }
 }
 
-fn loop_until_valid_proof(
-    membership_size: usize,
-    local_node_index: Option<usize>,
-) -> BlendLayerProof {
-    // For tests, we avoid generating proofs that are addressed to the local node
-    // itself, since in most tests there are only two nodes and those payload would
-    // fail to be propagated.
-    // This is not a precaution we need to consider in production, since there will
-    // be a minimum network size that is larger than 2.
+fn loop_until_valid_proof() -> BlendLayerProof {
     loop {
         let Ok(proof_of_quota) =
             ProofOfQuota::from_bytes(&random_sized_bytes::<{ size_of::<ProofOfQuota>() }>()[..])
@@ -167,16 +133,11 @@ fn loop_until_valid_proof(
         ) else {
             continue;
         };
-        let Ok(expected_index) = proof_of_selection.expected_index(membership_size) else {
-            continue;
+        return BlendLayerProof {
+            ephemeral_signing_key: Ed25519PrivateKey::generate(),
+            proof_of_quota,
+            proof_of_selection,
         };
-        if Some(expected_index) != local_node_index {
-            return BlendLayerProof {
-                ephemeral_signing_key: Ed25519PrivateKey::generate(),
-                proof_of_quota,
-                proof_of_selection,
-            };
-        }
     }
 }
 
