7cbe82fd351e7ca53541a8cc58298eb6f48129f7
megonen <146561843+megonen@users.noreply.github.com>
2025-11-11 22:11:19 +0300

    fix(da): Align rs decode domain (#1897)
    Co-authored-by: danielSanchezQ <3danimanimal@gmail.com>

--- nomos-da/kzgrs/benches/rs.rs
+++ nomos-da/kzgrs/benches/rs.rs
@@ -3,7 +3,7 @@ use ark_poly::{EvaluationDomain as _, GeneralEvaluationDomain};
 use divan::{Bencher, black_box, counter::BytesCount};
 use kzgrs::{
     common::bytes_to_polynomial,
-    rs::{decode, encode},
+    rs::{decode_unchecked, encode},
 };
 use rand::{RngCore as _, thread_rng};
 
@@ -43,10 +43,11 @@ fn rs_decode(bencher: Bencher, size: usize) {
         .bench_values(|buff| {
             black_box(move || {
                 let domain = GeneralEvaluationDomain::<Fr>::new(size).unwrap();
+                let expanded_domain = GeneralEvaluationDomain::<Fr>::new(size * 2).unwrap();
                 let missing_data: Vec<_> = std::iter::repeat_n(None, size)
                     .chain(buff.evals[size..].iter().copied().map(Some))
                     .collect();
-                decode(size, &missing_data, domain)
+                decode_unchecked(size, &missing_data, domain, expanded_domain)
             })
         });
 }
