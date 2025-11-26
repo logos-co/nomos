fc26a42a51426434b128569f2aedae8aa39924ef
Antonio Antonino <antonio@status.im>
2025-11-26 10:57:34 +0200

    nomos-blend-message review

--- nomos-blend/message/src/crypto/key_ext.rs
+++ nomos-blend/message/src/crypto/key_ext.rs
@@ -1,6 +1,10 @@
 use key_management_system_keys::keys::Ed25519Key;
 use nomos_blend_crypto::keys::X25519PrivateKey;
 
+// This extension trait must go here instead of `nomos-blend-crypto` because
+// else we would have a circular dependency between that and
+// `key-management-system-keys`. Also, these extension functions are mostly used
+// in this crate, so it makes most sense for them to be defined here.
 pub trait Ed25519SecretKeyExt {
     fn derive_x25519(&self) -> X25519PrivateKey;
 }
