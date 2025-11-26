e0a9b2369207132e5a6641492b449c5627ae09a1
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-11-26 16:50:55 +0100

    Implement cryptarchia and wallet bindings.

--- nomos-c/src/errors.rs
+++ nomos-c/src/errors.rs
@@ -5,3 +5,22 @@ pub enum NomosNodeErrorCode {
     StopError = 0x2,
     NullPtr = 0x3,
 }
+
+#[derive(PartialEq, Eq)]
+#[repr(u8)]
+pub enum OperationStatus {
+    Ok = 0x0,
+    NotFound = 0x1,
+    NullPtr = 0x2,
+    RelayError = 0x3,
+    ChannelSendError = 0x4,
+    ChannelReceiveError = 0x5,
+    ServiceError = 0x6,
+    RuntimeError = 0x7,
+}
+
+impl OperationStatus {
+    pub fn is_ok(&self) -> bool {
+        *self == Self::Ok
+    }
+}
