17e0c0ac917c2fb8b49fc87c1545a6055ae01b0c
Antonio Antonino <antonio@status.im>
2025-11-26 12:00:20 +0200

    Add zeroize bound on Ed25519 secret key extension trait

--- nomos-blend/message/src/crypto/key_ext.rs
+++ nomos-blend/message/src/crypto/key_ext.rs
@@ -1,11 +1,12 @@
 use key_management_system_keys::keys::Ed25519Key;
 use nomos_blend_crypto::keys::X25519PrivateKey;
+use zeroize::ZeroizeOnDrop;
 
 // This extension trait must go here instead of `nomos-blend-crypto` because
 // else we would have a circular dependency between that and
 // `key-management-system-keys`. Also, these extension functions are mostly used
 // in this crate, so it makes most sense for them to be defined here.
-pub trait Ed25519SecretKeyExt {
+pub trait Ed25519SecretKeyExt: ZeroizeOnDrop {
     fn derive_x25519(&self) -> X25519PrivateKey;
 }
 
