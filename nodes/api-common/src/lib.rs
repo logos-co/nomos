c36c05483e1de9ec470a01a82ffd153288250df6
Álex <alex93cabeza@gmail.com>
2025-12-04 12:37:15 +0100

    feat(wallet): HTTP API (#1939)

--- nodes/api-common/src/lib.rs
+++ nodes/api-common/src/lib.rs
@@ -1,6 +1,6 @@
+pub mod bodies;
 pub mod paths;
 #[cfg(feature = "profiling")]
 pub mod pprof;
 pub mod settings;
-pub mod types;
 pub mod utils;
--- nodes/nomos-node/node/src/api/responses/mod.rs
+++ nodes/nomos-node/node/src/api/responses/mod.rs
@@ -1,1 +1,2 @@
 pub mod ndjson;
+pub mod overwatch;
