440fa627f0027ca604c37e07d478cf97882756f5
Daniel <sanchez.quiros.daniel@gmail.com>
2025-12-10 11:38:02 +0100

    First attempt with random roots per sub tree

--- nomos-services/chain/chain-leader/benches/pol_sk.rs
+++ nomos-services/chain/chain-leader/benches/pol_sk.rs
@@ -10,17 +10,27 @@ fn main() {
     divan::main();
 }
 
-#[divan::bench(sample_count = 1, sample_size = 1)]
+#[divan::bench(sample_count = 10, sample_size = 10)]
 fn precompute_slot_secret(bencher: Bencher) {
     bencher
         .with_inputs(|| {
             let seed: Fr = fr_from_bytes(b"1987").unwrap();
             seed
         })
-        .bench_values(|seed| black_box(MerklePolCache::new(seed, 25, 20)));
+        .bench_values(|seed| black_box(MerklePolCache::new(seed, Slot::new(0), 25, 20)));
 }
 
-#[divan::bench(sample_count = 1, sample_size = 1)]
+#[divan::bench(sample_count = 10, sample_size = 10)]
+fn compute_non_cached_subtree(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            let seed: Fr = fr_from_bytes(b"1987").unwrap();
+            seed
+        })
+        .bench_values(|seed| black_box(MerklePolSubtree::new(seed, 5).merkle_path_for_index(0)));
+}
+
+#[divan::bench(sample_count = 10, sample_size = 10)]
 fn precompute_leaves(bencher: Bencher) {
     bencher
         .with_inputs(|| {
@@ -30,7 +40,7 @@ fn precompute_leaves(bencher: Bencher) {
         .bench_values(|seed| {
             black_box(
                 MerklePolCache::leaves_from_seed(seed)
-                    .take(2usize.pow(25))
+                    .take(2usize.pow(20))
                     .collect::<Vec<_>>(),
             )
         });
--- nomos-services/chain/chain-leader/src/pol/merkle.rs
+++ nomos-services/chain/chain-leader/src/pol/merkle.rs
@@ -1,7 +1,12 @@
+use std::collections::VecDeque;
+
 use cryptarchia_engine::Slot;
-use groth16::Fr;
+use groth16::{Fr, fr_from_bytes, fr_from_bytes_unchecked};
 use nomos_core::crypto::{ZkDigest, ZkHasher};
-use rayon::iter::{IntoParallelIterator as _, ParallelExtend as _, ParallelIterator as _};
+use rand::{RngCore, random};
+use rayon::iter::{
+    IntoParallelIterator as _, IntoParallelRefIterator, ParallelExtend as _, ParallelIterator as _,
+};
 
 use crate::pol::SlotSecret;
 
@@ -10,7 +15,7 @@ pub type CachedTree = Vec<Vec<Fr>>;
 #[derive(Clone)]
 pub struct MerklePolCache {
     pub cached_tree: CachedTree,
-    pub lower_merkle_pol: MerklePolSubtree,
+    pub lower_sub_trees: Vec<MerklePolSubtree>,
     pub tree_depth: usize,
     pub current_index: u64,
     pub starting_slot: Slot,
@@ -19,32 +24,29 @@ pub struct MerklePolCache {
 impl MerklePolCache {
     #[must_use]
     pub fn new(seed: Fr, starting_slot: Slot, tree_depth: usize, cache_depth: usize) -> Self {
-        let numer_of_trees = 2usize.pow(cache_depth as u32);
-        let total_leaves = 2usize.pow(tree_depth as u32);
-        let seed_jump = total_leaves / numer_of_trees;
+        let sub_tree_depth = tree_depth - cache_depth;
+        let numer_of_sub_trees = 2usize.pow(sub_tree_depth as u32);
         // we have to pre-cache the leaves so we can parallelize the subtree computation
-        let seeds: Vec<Fr> = Self::leaves_from_seed(seed).take(total_leaves).collect();
-        // we just recompute the first tree because after we have computed the root
-        // secret, we will compute a subtree at a time for each slot secret path
-        let first_tree = MerklePolSubtree::from_leafs(seeds.iter().take(seed_jump).copied());
-        // to parallelize the computation of the lower tree roots,
-        // we need to pre-cache the slices of the leaves to use par-iter
-        let seeds_chunks: Vec<&[Fr]> = seeds.chunks(seed_jump).skip(1).collect();
-        let mut lower_tree_roots = Vec::with_capacity(numer_of_trees);
-        lower_tree_roots.push(first_tree.secret_root());
-        lower_tree_roots.par_extend(
-            seeds_chunks
-                .into_par_iter()
-                .map(|leafs| MerklePolSubtree::from_leafs(leafs.iter().copied()).secret_root()),
-        );
-        lower_tree_roots.reverse();
+        let lower_sub_trees: Vec<_> = Self::leaves_from_seed(seed)
+            .take(numer_of_sub_trees)
+            .map(|seed| MerklePolSubtree::new(seed, sub_tree_depth))
+            .collect();
+        let lower_tree_roots: Vec<Fr> = lower_sub_trees
+            .par_iter()
+            .map(|subtree| {
+                *subtree
+                    .merkle_path_for_index(0)
+                    .first()
+                    .expect("Root should be always present")
+            })
+            .collect();
 
         // compute the root slot secret
-        let cached_tree = compute_cached_tree_from_leafs(lower_tree_roots);
+        let cached_tree = compute_cached_tree_from_leafs(&lower_tree_roots);
 
         Self {
             cached_tree,
-            lower_merkle_pol: first_tree,
+            lower_sub_trees,
             tree_depth,
             starting_slot,
             current_index: 0,
@@ -52,7 +54,7 @@ impl MerklePolCache {
     }
 
     pub fn leaves_from_seed(seed: Fr) -> impl Iterator<Item = Fr> {
-        std::iter::successors(Some(seed), |seed| Some(ZkHasher::digest(&[*seed])))
+        std::iter::repeat_with(|| fr_from_bytes_unchecked(&random::<[u8; 31]>()))
     }
 
     #[must_use]
@@ -75,7 +77,7 @@ impl MerklePolCache {
     pub fn merkle_path_for_index(&self, index: usize) -> Vec<Fr> {
         let mut cached_path = get_merkle_path(&self.cached_tree, index, self.cached_tree.len());
         // TODO: chose the proper tree depending the index
-        let current_path = self.lower_merkle_pol.merkle_path_for_index(index);
+        let current_path = self.lower_sub_trees[0].merkle_path_for_index(index);
         cached_path.extend(current_path);
         cached_path
     }
@@ -83,41 +85,34 @@ impl MerklePolCache {
 
 #[derive(Clone)]
 pub struct MerklePolSubtree {
-    merkle_tree: CachedTree,
+    seed: Fr,
+    tree_depth: usize,
 }
 
 impl MerklePolSubtree {
     #[must_use]
-    pub fn new(seed: Fr, tree_depth: usize) -> Self {
-        let hashed_leafs =
-            std::iter::successors(Some(seed), |seed| Some(ZkHasher::digest(&[*seed])))
-                .take(2usize.pow(tree_depth as u32));
-        Self::from_leafs(hashed_leafs)
-    }
-
-    pub fn from_leafs(leafs: impl Iterator<Item = Fr>) -> Self {
-        let merkle_tree = compute_cached_tree_from_leafs(leafs.collect());
-        Self { merkle_tree }
+    pub const fn new(seed: Fr, tree_depth: usize) -> Self {
+        Self { seed, tree_depth }
     }
 
     #[must_use]
     pub fn merkle_path_for_index(&self, index: usize) -> Vec<Fr> {
-        get_merkle_path(&self.merkle_tree, index, self.depth())
+        let hashed_leafs: Vec<_> =
+            std::iter::successors(Some(self.seed), |seed| Some(ZkHasher::digest(&[*seed])))
+                .take(2usize.pow(self.tree_depth as u32))
+                .collect();
+        let merkle_tree = compute_cached_tree_from_leafs(&hashed_leafs);
+        get_merkle_path(&merkle_tree, index, self.depth())
     }
 
     #[must_use]
     pub const fn depth(&self) -> usize {
-        self.merkle_tree.len()
-    }
-
-    #[must_use]
-    pub fn secret_root(&self) -> Fr {
-        self.merkle_tree[0][0]
+        self.tree_depth
     }
 }
 
-fn compute_cached_tree_from_leafs(tree_leafs: Vec<Fr>) -> Vec<Vec<Fr>> {
-    let mut cached_tree: Vec<Vec<Fr>> = std::iter::successors(Some(tree_leafs), |leafs| {
+fn compute_cached_tree_from_leafs(tree_leafs: &[Fr]) -> Vec<Vec<Fr>> {
+    let mut cached_tree: Vec<Vec<Fr>> = std::iter::successors(Some(tree_leafs.to_vec()), |leafs| {
         if leafs.len() <= 1 {
             return None;
         }
@@ -167,42 +162,44 @@ mod test {
     use rand::Rng as _;
 
     use crate::pol::merkle::MerklePolSubtree;
-
-    #[test]
-    fn new_roots_are_consistent() {
-        let merkle_pol = MerklePolSubtree::new(fr_from_bytes(b"1987").unwrap(), 8);
-        assert_eq!(merkle_pol.merkle_proof.len(), 9);
-        let merkle_proof_root = merkle_pol
-            .merkle_proof
-            .iter()
-            .copied()
-            .reduce(|a, b| <ZkHasher as ZkDigest>::compress(&[a, b]))
-            .unwrap();
-        assert_eq!(merkle_proof_root, merkle_pol.slot_secret_root);
-    }
-
-    #[test]
-    fn derive_next_proof() {
-        let mut merkle_pol = MerklePolSubtree::new(fr_from_bytes(b"1987").unwrap(), 8);
-        let mut rng = rand::thread_rng();
-        for _ in 0..rng.gen_range(2..250) {
-            merkle_pol = merkle_pol.next_merkle();
-        }
-
-        let mut merkle_proof_root = merkle_pol.merkle_proof[0];
-        for i in 0..8 {
-            if (u64::from(merkle_pol.current_index) >> i) & 1 == 1 {
-                merkle_proof_root = <ZkHasher as ZkDigest>::compress(&[
-                    merkle_pol.merkle_proof[i + 1],
-                    merkle_proof_root,
-                ]);
-            } else {
-                merkle_proof_root = <ZkHasher as ZkDigest>::compress(&[
-                    merkle_proof_root,
-                    merkle_pol.merkle_proof[i + 1],
-                ]);
-            }
-        }
-        assert_eq!(merkle_proof_root, merkle_pol.slot_secret_root);
-    }
+    //
+    // #[test]
+    // fn new_roots_are_consistent() {
+    //     let merkle_pol =
+    // MerklePolSubtree::new(fr_from_bytes(b"1987").unwrap(), 8);
+    //     assert_eq!(merkle_pol.merkle_proof.len(), 9);
+    //     let merkle_proof_root = merkle_pol
+    //         .merkle_proof
+    //         .iter()
+    //         .copied()
+    //         .reduce(|a, b| <ZkHasher as ZkDigest>::compress(&[a, b]))
+    //         .unwrap();
+    //     assert_eq!(merkle_proof_root, merkle_pol.slot_secret_root);
+    // }
+    //
+    // #[test]
+    // fn derive_next_proof() {
+    //     let mut merkle_pol =
+    // MerklePolSubtree::new(fr_from_bytes(b"1987").unwrap(), 8);
+    //     let mut rng = rand::thread_rng();
+    //     for _ in 0..rng.gen_range(2..250) {
+    //         merkle_pol = merkle_pol.next_merkle();
+    //     }
+    //
+    //     let mut merkle_proof_root = merkle_pol.merkle_proof[0];
+    //     for i in 0..8 {
+    //         if (u64::from(merkle_pol.current_index) >> i) & 1 == 1 {
+    //             merkle_proof_root = <ZkHasher as ZkDigest>::compress(&[
+    //                 merkle_pol.merkle_proof[i + 1],
+    //                 merkle_proof_root,
+    //             ]);
+    //         } else {
+    //             merkle_proof_root = <ZkHasher as ZkDigest>::compress(&[
+    //                 merkle_proof_root,
+    //                 merkle_pol.merkle_proof[i + 1],
+    //             ]);
+    //         }
+    //     }
+    //     assert_eq!(merkle_proof_root, merkle_pol.secret_root());
+    // }
 }
